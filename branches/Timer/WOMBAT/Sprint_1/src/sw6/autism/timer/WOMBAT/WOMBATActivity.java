8e16bab4a4e23fc71423ca8dc4f89e14adbffdc7
sbf.aau@gmail.com <sbf.aau@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-19 10:03:24 +0000

    Developer Name: Simon Frandsen Synopsis: Added new development project

---
+++ branches/Timer/WOMBAT/Sprint_1/src/sw6/autism/timer/WOMBAT/WOMBATActivity.java
@@ -0,0 +1,13 @@
+package sw6.autism.timer.WOMBAT;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WOMBATActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
