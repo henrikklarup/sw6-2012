e3ea0e816ecbf60ceae6787f04da0f5f52e5ed21
jensmohr90@gmail.com <jensmohr90@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-19 14:11:33 +0000

    slå fejl rettet

--- AutismLocalDatabase/src/sw6/autism/admin/provider/AppsTable.java
+++ AutismLocalDatabase/src/sw6/autism/admin/provider/AppsTable.java
@@ -9,7 +9,7 @@ public class AppsTable {
 			+ "("
 			+ AppsMetaData.Table.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
 			+ AppsMetaData.Table.COLUMN_NAME + " TEXT NOT NULL, "
-			+ AppsMetaData.Table.COLUMN_VERSIONNUMBER + " INTEGER NOT NULL"
+			+ AppsMetaData.Table.COLUMN_VERSIONNUMBER + " TEXT NOT NULL"
 			+ ");";
 	
 	private static final String TABLE_DROP= "DROP TABLE IF EXISTS " + AppsMetaData.Table.TABLE_NAME + ";";
