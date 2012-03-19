1c1acca3caa48a75fbcd82cf48834c20f17c1dc6
sbf.aau@gmail.com <sbf.aau@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-19 20:59:54 +0000

    Developer Name: Simon Frandsen Synopsis: Created listView

--- branches/Timer/WOMBAT/Sprint_1/gen/sw6/autism/timer/WOMBAT/R.java
+++ branches/Timer/WOMBAT/Sprint_1/gen/sw6/autism/timer/WOMBAT/R.java
@@ -8,21 +8,19 @@
 package sw6.autism.timer.WOMBAT;
 
 public final class R {
+    public static final class array {
+        public static final int profiles_array=0x7f050000;
+        public static final int templates=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
-    public static final class id {
-        public static final int mylist=0x7f050000;
-    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int customized=0x7f040003;
-        public static final int hello=0x7f040000;
-        public static final int standard=0x7f040002;
+        public static final int app_name=0x7f040000;
     }
 }
--- branches/Timer/WOMBAT/Sprint_1/src/sw6/autism/timer/WOMBAT/WOMBATActivity.java
+++ branches/Timer/WOMBAT/Sprint_1/src/sw6/autism/timer/WOMBAT/WOMBATActivity.java
@@ -1,15 +1,21 @@
 package sw6.autism.timer.WOMBAT;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.app.ListActivity;
-//import android.widget.ListAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class WOMBATActivity extends ListActivity {
     /** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
+	  
+	  String[] profiles = getResources().getStringArray(R.array.profiles_array);
+	  setListAdapter(new ArrayAdapter<String>(this, R.layout.main, profiles));
 
-	}
+	  ListView lv = getListView();
+	  lv.setTextFilterEnabled(true);
+	  }
 }
