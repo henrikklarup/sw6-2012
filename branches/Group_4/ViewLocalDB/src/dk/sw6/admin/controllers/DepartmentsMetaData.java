6cd46d05e59999e6a1dd968720632be03d367727
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-19 12:04:32 +0000

    Developer Name: Dan Møller Synopsis: Created metadata files

---
+++ branches/Group_4/ViewLocalDB/src/dk/sw6/admin/controllers/DepartmentsMetaData.java
@@ -0,0 +1,17 @@
+package dk.sw6.admin.controllers;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class DepartmentsMetaData {
+	
+	public static final Uri CONTENT_URI = Uri.parse("content://sw6.autism.admin.provider.AutismProvider/departments");
+	
+	public class DepartmentsTable implements BaseColumns {
+		public static final String TABLE_NAME = "tbl_departments";
+		
+		public static final String COLUMN_ID = "_id";
+		public static final String COLUMN_NAME = "departments_name";
+		public static final String COLUMN_PHONE = "departments_phone";
+	}
+}
