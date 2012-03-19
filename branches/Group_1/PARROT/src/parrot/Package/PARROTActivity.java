038abb21562dea35638df9697bbd38a56d40560f
rasmusdalhoffjensen@hotmail.com <rasmusdalhoffjensen@hotmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-19 12:24:12 +0000

    Developer Name: Synopsis:

---
+++ branches/Group_1/PARROT/src/parrot/Package/PARROTActivity.java
@@ -0,0 +1,14 @@
+package parrot.Package;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class PARROTActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
