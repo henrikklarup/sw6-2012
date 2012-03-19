e59365b2817de6b53463a8c13cd3f86031e08643
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-19 14:09:24 +0000

    Developer Name: Dan Møller Synopsis: Created ListOfApps viewmodel

--- branches/Group_4/ViewLocalDB/src/dk/sw6/admin/viewmodels/ListOfApps.java
+++ branches/Group_4/ViewLocalDB/src/dk/sw6/admin/viewmodels/ListOfApps.java
@@ -1,5 +1,58 @@
 package dk.sw6.admin.viewmodels;
 
 public class ListOfApps {
-
+	private long appId;
+	private long profileId;
+	private String settings;
+	private String stats;
+	/**
+	 * @return the appId
+	 */
+	public long getAppId() {
+		return appId;
+	}
+	/**
+	 * @param appId the appId to set
+	 */
+	public void setAppId(long appId) {
+		this.appId = appId;
+	}
+	/**
+	 * @return the profileId
+	 */
+	public long getProfileId() {
+		return profileId;
+	}
+	/**
+	 * @param profileId the profileId to set
+	 */
+	public void setProfileId(long profileId) {
+		this.profileId = profileId;
+	}
+	/**
+	 * @return the settings
+	 */
+	public String getSettings() {
+		return settings;
+	}
+	/**
+	 * @param settings the settings to set
+	 */
+	public void setSettings(String settings) {
+		this.settings = settings;
+	}
+	/**
+	 * @return the stats
+	 */
+	public String getStats() {
+		return stats;
+	}
+	/**
+	 * @param stats the stats to set
+	 */
+	public void setStats(String stats) {
+		this.stats = stats;
+	}
+	
+	
 }
