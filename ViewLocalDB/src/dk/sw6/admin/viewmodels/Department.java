c4901fd2d43a8c13d79f2d59e95a2a9ae5288f97
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-19 12:38:53 +0000

    Developer Name: Dan Møller Synopsis: Created various view models

--- ViewLocalDB/src/dk/sw6/admin/viewmodels/Department.java
+++ ViewLocalDB/src/dk/sw6/admin/viewmodels/Department.java
@@ -1,17 +1,70 @@
 package dk.sw6.admin.viewmodels;
 
-import android.net.Uri;
-import android.provider.BaseColumns;
-
 public class Department {
 	
-	public static final Uri CONTENT_URI = Uri.parse("content://sw6.autism.admin.provider.AutismProvider/departments");
-	
-	public class DepartmentsTable implements BaseColumns {
-		public static final String TABLE_NAME = "tbl_departments";
-		
-		public static final String COLUMN_ID = "_id";
-		public static final String COLUMN_NAME = "departments_name";
-		public static final String COLUMN_PHONE = "departments_phone";
+	private long id;
+	private String name;
+	private String address;
+	private long phone;
+	private String email;
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the phone
+	 */
+	public long getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
--- ViewLocalDB/src/dk/sw6/admin/viewmodels/Profile.java
+++ ViewLocalDB/src/dk/sw6/admin/viewmodels/Profile.java
@@ -1,17 +1,109 @@
 package dk.sw6.admin.viewmodels;
 
-import android.net.Uri;
-import android.provider.BaseColumns;
-
 public class Profile {
 
-	public static final Uri CONTENT_URI = Uri.parse("content://sw6.autism.admin.provider.AutismProvider/profiles");
-	
-	public class ProfilesTable implements BaseColumns {
-		public static final String TABLE_NAME = "tbl_profiles";
-		
-		public static final String COLUMN_ID = "_id";
-		public static final String COLUMN_FIRST_NAME = "profile_first_name";
-		public static final String COLUMN_ROLE = "profile_role";
+	private long id;
+	private String name;
+	private String surname;
+	private String middlename;
+	private long role;
+	private long phone;
+	private String picture;
+	private long departmentId;
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	/**
+	 * @return the middlename
+	 */
+	public String getMiddlename() {
+		return middlename;
+	}
+	/**
+	 * @param middlename the middlename to set
+	 */
+	public void setMiddlename(String middlename) {
+		this.middlename = middlename;
+	}
+	/**
+	 * @return the role
+	 */
+	public long getRole() {
+		return role;
+	}
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(long role) {
+		this.role = role;
+	}
+	/**
+	 * @return the phone
+	 */
+	public long getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the picture
+	 */
+	public String getPicture() {
+		return picture;
+	}
+	/**
+	 * @param picture the picture to set
+	 */
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	/**
+	 * @return the departmentId
+	 */
+	public long getDepartmentId() {
+		return departmentId;
+	}
+	/**
+	 * @param departmentId the departmentId to set
+	 */
+	public void setDepartmentId(long departmentId) {
+		this.departmentId = departmentId;
 	}
 }
