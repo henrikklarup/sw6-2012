9ea1155ae84c40f4105fcdb76ef4b0ef30f2c0d7
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-21 12:22:12 +0000

    Developer Name: Dan Møller Synopsis: Added a test suite to the oasislib.

---
+++ OasisLib/src/sw6/oasis/controllers/test/DepartmentsHelperTest.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package sw6.oasis.controllers.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Dan Møller
+ *
+ */
+public class DepartmentsHelperTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.DepartmentsHelper#DepartmentsHelper(android.content.Context)}.
+	 */
+	public void testDepartmentsHelper() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.DepartmentsHelper#insertDepartment(sw6.oasis.viewmodels.Department)}.
+	 */
+	public void testInsertDepartment() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.DepartmentsHelper#modifyDepartment(sw6.oasis.viewmodels.Department)}.
+	 */
+	public void testModifyDepartment() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.DepartmentsHelper#getDepartments()}.
+	 */
+	public void testGetDepartments() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.DepartmentsHelper#clearDepartmentsTable()}.
+	 */
+	public void testClearDepartmentsTable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
