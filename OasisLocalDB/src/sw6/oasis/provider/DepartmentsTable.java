86edf3902009f3fb8d99d549535fd0e9f20cea31
jensmohr90@gmail.com <jensmohr90@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-21 14:21:13 +0000

    documentation

--- OasisLocalDB/src/sw6/oasis/provider/DepartmentsTable.java
+++ OasisLocalDB/src/sw6/oasis/provider/DepartmentsTable.java
@@ -15,10 +15,20 @@ public class DepartmentsTable {
 	
 	private static final String TABLE_DROP= "DROP TABLE IF EXISTS " + DepartmentsMetaData.Table.TABLE_NAME + ";";
 
+	/**
+	 * Executes a sql string for creating a new departments table
+	 * @param db this is an instance of a sqlite database
+	 */
 	public static void onCreate(SQLiteDatabase db) {
 		db.execSQL(TABLE_CREATE);
 	}
 
+	/**
+	 * Executes a sql string which drops the current table and then the methods calls oncreate, which creates a new table
+	 * @param db this is an instance of a sqlite database
+	 * @param oldVersion integer refering to the old version number
+	 * @param newVersion integer refering to the new version number
+	 */
 	public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		db.execSQL(TABLE_DROP);
 		onCreate(db);
--- OasisLocalDB/src/sw6/oasis/provider/ListOfAppsTable.java
+++ OasisLocalDB/src/sw6/oasis/provider/ListOfAppsTable.java
@@ -16,10 +16,20 @@ public class ListOfAppsTable {
 
 	private static final String TABLE_DROP= "DROP TABLE IF EXISTS " + ListOfAppsMetaData.Table.TABLE_NAME + ";";
 
+	/**
+	 * Executes a sql string, which creates a new listofapps table
+	 * @param db this is an instance of a sqlite database
+	 */
 	public static void onCreate(SQLiteDatabase db) {
 		db.execSQL(TABLE_CREATE);
 	}
 
+	/**
+	 * Executes a sql string, which drop the current table, then the mehod calls the oncreate, which creates a new table
+	 * @param db this is an instance of a sqlite database
+	 * @param oldVersion integer refering to the old version number
+	 * @param newVersion integer refering to the new version number
+	 */
 	public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		db.execSQL(TABLE_DROP);
 		onCreate(db);
