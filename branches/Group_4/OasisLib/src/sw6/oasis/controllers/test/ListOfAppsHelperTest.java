696155b85338f7b5582dc9b6e8ddecce89f761a4
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-21 12:22:12 +0000

    Developer Name: Dan Møller Synopsis: Added a test suite to the oasislib.

---
+++ branches/Group_4/OasisLib/src/sw6/oasis/controllers/test/ListOfAppsHelperTest.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package sw6.oasis.controllers.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Dan Møller
+ *
+ */
+public class ListOfAppsHelperTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.ListOfAppsHelper#ListOfAppsHelper(android.content.Context)}.
+	 */
+	public void testListOfAppsHelper() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.ListOfAppsHelper#insertListOfApps(sw6.oasis.viewmodels.ListOfApps)}.
+	 */
+	public void testInsertListOfApps() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.ListOfAppsHelper#modifyListOfApps(sw6.oasis.viewmodels.ListOfApps)}.
+	 */
+	public void testModifyListOfApps() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.ListOfAppsHelper#getListOfApps()}.
+	 */
+	public void testGetListOfApps() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sw6.oasis.controllers.ListOfAppsHelper#clearListOfAppsTable()}.
+	 */
+	public void testClearListOfAppsTable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
