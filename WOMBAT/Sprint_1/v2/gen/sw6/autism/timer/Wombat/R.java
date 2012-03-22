d4ad312d191e052e9f9e7d0880d83b492809992d
rasmus5034@gmail.com <rasmus5034@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-22 11:57:56 +0000

    Developer Name: Rasmus A Synopsis: Updated Wombat according to layout

--- WOMBAT/Sprint_1/v2/gen/sw6/autism/timer/Wombat/R.java
+++ WOMBAT/Sprint_1/v2/gen/sw6/autism/timer/Wombat/R.java
@@ -18,10 +18,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int customizeFragment=0x7f060003;
-        public static final int detailFragment=0x7f060002;
+        public static final int customizeFragment=0x7f060006;
+        public static final int customizeHeader=0x7f060005;
+        public static final int detailFragment=0x7f060004;
         public static final int detailsText=0x7f060000;
-        public static final int listFragment=0x7f060001;
+        public static final int listFragment=0x7f060002;
+        public static final int profileListHeader=0x7f060001;
+        public static final int settingsListHeader=0x7f060003;
     }
     public static final class layout {
         public static final int details=0x7f030000;
@@ -29,6 +32,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int customize_header=0x7f050004;
+        public static final int profile_list_header=0x7f050002;
         public static final int start_activity_default=0x7f050001;
+        public static final int template_list_header=0x7f050003;
     }
 }
