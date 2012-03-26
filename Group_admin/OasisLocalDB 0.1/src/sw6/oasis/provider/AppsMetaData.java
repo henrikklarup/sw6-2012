ba3fcb17128131f0602e2eafbc1cb3f789753888
henrikklarup@gmail.com <henrikklarup@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-26 06:51:46 +0000

    Developer Name: Henrik Synopsis: OasisLocalDB 0.1

---
+++ Group_admin/OasisLocalDB 0.1/src/sw6/oasis/provider/AppsMetaData.java
@@ -0,0 +1,20 @@
+package sw6.oasis.provider;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class AppsMetaData {
+	
+	public static final Uri CONTENT_URI = Uri.parse("content://sw6.oasis.provider.AutismProvider/apps");
+	
+	public static final String CONTENT_TYPE_APPS_LIST = "vnd.android.cursor.dir/vnd.sw6.apps";
+	public static final String CONTENT_TYPE_APP_ONE = "vnd.android.cursor.item/vnd.sw6.apps";
+	
+	public class Table implements BaseColumns {
+		public static final String TABLE_NAME = "tbl_apps";
+		
+		public static final String COLUMN_ID = "_id";
+		public static final String COLUMN_NAME = "apps_name";
+		public static final String COLUMN_VERSIONNUMBER = "apps_version";
+	}
+}
