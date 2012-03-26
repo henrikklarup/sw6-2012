d7cfd2cc15f2cacb5face6220b382ef5ec0b6fce
henrikklarup@gmail.com <henrikklarup@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-26 06:51:46 +0000

    Developer Name: Henrik Synopsis: OasisLocalDB 0.1

---
+++ trunk/Group_admin/OasisLocalDB 0.1/src/sw6/oasis/provider/AppsTable.java
@@ -0,0 +1,35 @@
+package sw6.oasis.provider;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class AppsTable {
+	
+	private static final String TABLE_CREATE = "CREATE TABLE "
+			+ AppsMetaData.Table.TABLE_NAME
+			+ "("
+			+ AppsMetaData.Table.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ AppsMetaData.Table.COLUMN_NAME + " TEXT NOT NULL, "
+			+ AppsMetaData.Table.COLUMN_VERSIONNUMBER + " TEXT NOT NULL"
+			+ ");";
+	
+	private static final String TABLE_DROP= "DROP TABLE IF EXISTS " + AppsMetaData.Table.TABLE_NAME + ";";
+
+	/**
+	 * Executes sql string for creating app table
+	 * @param db this is an instance of a sqlite database
+	 */
+	public static void onCreate(SQLiteDatabase db) {
+		db.execSQL(TABLE_CREATE);
+	}
+
+	/**
+	 * executes a sql string which drops the old table and then the method call the oncreate, which creates a new app table
+	 * @param db this is an instance of a sqlite database
+	 * @param oldVersion integer refering to the old version number
+	 * @param newVersion integer refering to the new version number
+	 */
+	public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL(TABLE_DROP);
+		onCreate(db);
+	}
+}
