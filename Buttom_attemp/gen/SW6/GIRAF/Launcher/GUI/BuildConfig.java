1609baec97e8c0ebd1eab29d30447e5829e452ab
call911c@gmail.com <call911c@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-26 11:39:37 +0000

    Developer Name: RK Synopsis: Buttom_attemp

---
+++ Buttom_attemp/gen/SW6/GIRAF/Launcher/GUI/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package SW6.GIRAF.Launcher.GUI;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Buttom_attemp/src/SW6/GIRAF/Launcher/GUI/Buttom_attempActivity.java
@@ -0,0 +1,34 @@
+package SW6.GIRAF.Launcher.GUI;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Buttom_attempActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        //This be spam
+        
+//        TextView helvetica = null;
+//        setFont(helvetica, "Helvetica.tff", R.id.button1);
+//        
+//        Button B = (Button) findViewById(R.id.button1);
+//        Typeface font = Typeface.createFromAsset(getAssets(), "HelveticaBold.ttf");
+//        B.setTypeface(font);
+        
+    }
+//    void setFont(TextView name, String path, int res)
+//    {
+//    	name=(TextView)findViewById(res);
+//        Typeface font = Typeface.createFromAsset(this.getAssets(), path); 
+//
+//        name.setTypeface(font); 
+//
+//    }
+}
