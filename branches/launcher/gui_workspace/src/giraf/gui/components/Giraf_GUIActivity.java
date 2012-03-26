c58565898b3531085a69efabf7534603f6dfd58f
magnusstubman <magnusstubman@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-26 09:31:06 +0000

    hej henrik

---
+++ branches/launcher/gui_workspace/src/giraf/gui/components/Giraf_GUIActivity.java
@@ -0,0 +1,13 @@
+package giraf.gui.components;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Giraf_GUIActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
