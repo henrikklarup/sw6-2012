5a70e21a5a20f23c0bd8f675f982adbdf0205946
kristiank.flf@gmail.com <kristiank.flf@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-27 12:00:24 +0000

    Adding more features to our Lib

--- TimerLib/src/sw6/autism/timer/TimerLib/Child.java
+++ TimerLib/src/sw6/autism/timer/TimerLib/Child.java
@@ -2,11 +2,13 @@ package sw6.autism.timer.TimerLib;
 
 import java.util.ArrayList;
 
-public class Child extends Guardian{
+public class Child{
 	private ArrayList<Profile> profileList;
 	
-	public Child(){
+	public String _name;
+	public Child(String name){
 		profileList = new ArrayList<Profile>();
+		this._name = name;
 	}
 	
 	public ArrayList<Profile> Profiles(){
--- TimerLib/src/sw6/autism/timer/TimerLib/Guardian.java
+++ TimerLib/src/sw6/autism/timer/TimerLib/Guardian.java
@@ -1,14 +1,37 @@
 package sw6.autism.timer.TimerLib;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Stack;
+import java.util.Collections;
 
 public class Guardian {
 	
 	private static Guardian instance = null;
 	
-	private static ArrayList<Child> guard = null;
+	private ArrayList<Child> guard = null;
 	
-	public Guardian(){
+	private Stack<Profile> lastUsed = null;
+	
+	private ArrayList<Profile> predef = null;
+	
+	private ArrayList<Child> sortedList = null;
+	
+	public String _name;
+	
+	private Guardian(String name){
+		this._name = name;
+	}
+	
+	private Guardian(){	
+	}
+	
+	public static Guardian getInstance(String name){
+		if(instance == null){
+			instance = new Guardian(name);
+		}
+		return instance;
 	}
 	
 	public static Guardian getInstance(){
@@ -18,7 +41,7 @@ public class Guardian {
 		return instance;
 	}
 	
-	public static ArrayList<Child> init(){
+	public ArrayList<Child> initChildren(){
 		if(guard == null){
 			guard = new ArrayList<Child>();
 		}
@@ -26,10 +49,60 @@ public class Guardian {
 	}
 	
 	public ArrayList<Child> Children(){
+		if(guard == null){
+		guard = new ArrayList<Child>();
+		}
 		return guard;
 	}
+	
+	public Stack<Profile> lastUsed(){
+		if(lastUsed == null){
+		lastUsed = new Stack<Profile>();
+		}
+		return lastUsed;
+	}
+	
+	
+	//Waiting for admin
+	private void initLastUsed(ArrayList<Profile> profile){
+		for(Profile p : profile){
+			lastUsed.add(p);
+		}
+	}
+	
+	public void addLastUsed(Profile profile){
+		if(lastUsed.contains(profile)){
+			lastUsed.push(profile);
+		} else {
+			lastUsed.add(profile);
+		}
+	}
+	
+	public ArrayList<Profile> predefined(){
+		if(predef == null){
+		predef = new ArrayList<Profile>();
+		}
+		return predef;
+	}
+	
+	public ArrayList<Child> publishList(){
+		if(sortedList == null){
+			sortedList = new ArrayList<Child>();
+		}
+		Child lastUsedChild = new Child("Last Used");
+		lastUsedChild.Profiles().addAll(lastUsed);
+		
+		sortedList.add(lastUsedChild);
+		
+		Child predefChild = new Child("Predefined Profiles");
+		predefChild.Profiles().addAll(predef);
+		
+		sortedList.add(predefChild);
+		
+		
+		
+		return sortedList;
+	}
 
-	
-	
 
 }
