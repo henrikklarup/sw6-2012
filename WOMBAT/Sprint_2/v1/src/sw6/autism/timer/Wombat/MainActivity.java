169c751be68a2ff6d1e10552deee41692ab9d199
rasmus5034@gmail.com <rasmus5034@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-29 07:33:57 +0000

    Developer Name: Rasmus Aaen Synopsis: Added Pictures

--- WOMBAT/Sprint_2/v1/src/sw6/autism/timer/Wombat/MainActivity.java
+++ WOMBAT/Sprint_2/v1/src/sw6/autism/timer/Wombat/MainActivity.java
@@ -2,12 +2,15 @@ package sw6.autism.timer.Wombat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		 getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_HIDDEN);
 		// Set content view according to main, which implements two fragments
 		setContentView(R.layout.main);
+		
 	}
 }
