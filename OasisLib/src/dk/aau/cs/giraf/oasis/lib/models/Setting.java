92f055c46353c211404ddeb65ac4c3f2c3a8d922
henrikklarup@gmail.com <henrikklarup@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-29 12:32:31 +0000

    Developer Name: Henrik Synopsis: Added new settings model

--- OasisLib/src/dk/aau/cs/giraf/oasis/lib/models/Setting.java
+++ OasisLib/src/dk/aau/cs/giraf/oasis/lib/models/Setting.java
@@ -1,5 +1,25 @@
-package dk.aau.cs.giraf.oasis.lib.models;
+package sw6.oasis.models;
 
-public class Setting {
+import java.util.HashMap;
 
+public class Setting<Key, Type, Value> extends HashMap<Key, HashMap<Type, Value>>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Setting() {
+		super();
+	}
+	
+	public void addValue(Key key, Type type, Value value)
+	{
+		HashMap<Type, Value> typeMap = get(type);
+		if(typeMap == null) {
+			typeMap = new HashMap<Type, Value>();
+			put(key, typeMap);
+		}
+		typeMap.put(type, value);
+	}
 }
