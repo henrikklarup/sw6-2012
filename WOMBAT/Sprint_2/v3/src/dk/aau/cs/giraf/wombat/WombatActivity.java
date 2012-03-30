2e860b62bfe3633272e4572479ef369317e13261
rasmus5034@gmail.com <rasmus5034@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-03-30 10:16:44 +0000

    Developer Name: Rasmus Aaen Synopsis: Changed packagename

---
+++ WOMBAT/Sprint_2/v3/src/dk/aau/cs/giraf/wombat/WombatActivity.java
@@ -0,0 +1,13 @@
+package dk.aau.cs.giraf.wombat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WombatActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
