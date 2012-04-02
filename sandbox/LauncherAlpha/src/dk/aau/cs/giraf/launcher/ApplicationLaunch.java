d8feedece6770addbbbf87607ce2ebc3912e559f
call911c@gmail.com <call911c@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-02 11:29:53 +0000

    Developer Name: Rasmus Steiniche Synopsis: Changed class structure

--- sandbox/LauncherAlpha/src/dk/aau/cs/giraf/launcher/ApplicationLaunch.java
+++ sandbox/LauncherAlpha/src/dk/aau/cs/giraf/launcher/ApplicationLaunch.java
@@ -4,7 +4,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.app.Activity;
 
-	class IconClick extends Activity implements AdapterView.OnItemClickListener{
+	class ApplicationLaunch extends Activity implements AdapterView.OnItemClickListener{
         public void onItemClick(AdapterView parent, View v, int position, long id) {
             ApplicationInfo app = (ApplicationInfo) parent.getItemAtPosition(position);
             startActivity(app.intent);
--- sandbox/LauncherAlpha/src/dk/aau/cs/giraf/launcher/GiraffeLaunchEvent.java
+++
@@ -1,6 +0,0 @@
-package dk.aau.cs.giraf.launcher;
-
-import android.view.View;
-import android.widget.AdapterView;
-import android.app.Activity;
-
