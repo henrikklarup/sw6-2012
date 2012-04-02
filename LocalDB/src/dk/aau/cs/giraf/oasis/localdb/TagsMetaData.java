b8cfeb615107f49d1a079d8a2825e48ac920b211
jensmohr90@gmail.com <jensmohr90@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-02 19:16:05 +0000

    Hej Henrik, her har du et udkast til den nye lokale database

---
+++ LocalDB/src/dk/aau/cs/giraf/oasis/localdb/TagsMetaData.java
@@ -0,0 +1,20 @@
+package dk.aau.cs.giraf.oasis.localdb;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class TagsMetaData {
+
+	public static final Uri CONTENT_URI = Uri.parse("content://dk.aau.cs.giraf.oasis.localdb.AutismProvider/tags");
+	
+	public static final String CONTENT_TYPE_TAGS_LIST = "vnd.android.cursor.dir/vnd.sw6.tags";
+	public static final String CONTENT_TYPE_TAG_ONE = "vnd.android.cursor.item/vnd.sw6.tags";
+	
+	public class Table implements BaseColumns {
+		public static final String TABLE_NAME = "tbl_tags";
+		
+		public static final String COLUMN_ID = "_id";
+		public static final String COLUMN_CAPTION = "tags_caption";
+	}
+}
+
