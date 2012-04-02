e5f675f899e66360e15ed5103bbff407c8894a0f
henrikklarup@gmail.com <henrikklarup@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-02 12:38:38 +0000

    Developer Name: Henrik Synopsis: Setting & Stat to byte, and back

--- OasisLib/src/dk/aau/cs/giraf/oasis/lib/models/Setting.java
+++ OasisLib/src/dk/aau/cs/giraf/oasis/lib/models/Setting.java
@@ -1,5 +1,10 @@
 package dk.aau.cs.giraf.oasis.lib.models;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
 public class Setting<Key, Type, Value> extends HashMap<Key, HashMap<Type, Value>>{
@@ -21,5 +26,42 @@ public class Setting<Key, Type, Value> extends HashMap<Key, HashMap<Type, Value>
 			put(key, typeMap);
 		}
 		typeMap.put(type, value);
+	}
+	
+	public byte[] toByteArray (Setting<String, String, String> setting)
+	{
+	  Object obj = (Object)setting;
+	  byte[] bytes = null;
+	  ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	  try {
+	    ObjectOutputStream oos = new ObjectOutputStream(bos); 
+	    oos.writeObject(obj);
+	    oos.flush(); 
+	    oos.close(); 
+	    bos.close();
+	    bytes = bos.toByteArray ();
+	  }
+	  catch (IOException ex) {
+	    //TODO: Handle the exception
+	  }
+	  return bytes;
+	}
+	    
+	public Setting<String, String, String> toObject (byte[] bytes)
+	{
+	  Object obj = null;
+	  try {
+	    ByteArrayInputStream bis = new ByteArrayInputStream (bytes);
+	    ObjectInputStream ois = new ObjectInputStream (bis);
+	    obj = ois.readObject();
+	  }
+	  catch (IOException ex) {
+	    //TODO: Handle the exception
+	  }
+	  catch (ClassNotFoundException ex) {
+	    //TODO: Handle the exception
+	  }
+	  Setting<String, String, String> setting = (Setting<String, String, String>)obj;
+	  return setting;
 	}
 }
