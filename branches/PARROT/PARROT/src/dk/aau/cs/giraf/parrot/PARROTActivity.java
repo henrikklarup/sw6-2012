07407e8b17be890e97fcccafc3ecd078b325a978
rasmusdalhoffjensen@hotmail.com <rasmusdalhoffjensen@hotmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-03 13:50:51 +0000

    Developer Name: Rasmus D.C. Dalhoff-Jensen Synopsis: Added keystore

--- branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
+++ branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
@@ -18,7 +18,23 @@ public class PARROTActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
  	    
-        Category temp1 = null; //TODO dummy variable. Change this when database is up.
+        Category temp1 = new Category(0xff0000); //TODO dummy variable. Change this when database is up.
+        Pictogram pic = new Pictogram("001", "/root/pictures/001.jpg", null, null);
+        temp1.addPictogram(pic);
+        pic = new Pictogram("002", "/root/pictures/002.jpg", null, null);
+        temp1.addPictogram(pic);
+        pic = new Pictogram("003", "/root/pictures/003.jpg", null, null);
+        temp1.addPictogram(pic);
+        pic = new Pictogram("004", "/root/pictures/004.jpg", null, null);
+        temp1.addPictogram(pic);
+        pic = new Pictogram("005", "/root/pictures/005.jpg", null, null);
+        temp1.addPictogram(pic);
+        pic = new Pictogram("006", "/root/pictures/006.jpg", null, null);
+        temp1.addPictogram(pic);
+        pic = new Pictogram("007", "/root/pictures/007.jpg", null, null);
+        temp1.addPictogram(pic);
+        pic = new Pictogram("008", "/root/pictures/008.jpg", null, null);
+        temp1.addPictogram(pic);
          
         GridView superCategory = (GridView) findViewById(R.id.supercategory); //This is to show the supercategories
 		superCategory.setAdapter(new PictogramAdapter(temp1, this)); //FIXME
