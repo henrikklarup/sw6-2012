8b999067e6caabcd673ced914f5c47d2263680c1
kristiank.flf@gmail.com <kristiank.flf@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-03 10:19:48 +0000

    Developer Name: Synopsis:

--- TimerLib/src/dk/aau/cs/giraf/TimerLib/TimeTimer.java
+++ TimerLib/src/dk/aau/cs/giraf/TimerLib/TimeTimer.java
@@ -6,6 +6,5 @@ public class TimeTimer extends SubProfile {
 	public TimeTimer(int id, String name, String description, int size, int bgcolor, int timeLeftColor, int timeSpentColor, int frameColor, int totalTime, boolean changeColor){
 		super(id, name, description, size, bgcolor, timeLeftColor, timeSpentColor, frameColor, totalTime, changeColor);
 	}
-
 	
 }
