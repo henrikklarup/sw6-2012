476ef6f6f6c30b2bb5ecee6fb4e61bab1cc69f0a
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-03 05:56:42 +0000

    Developer Name: Dan Møller Synopsis: Updated the lib metadata

--- branches/Group_4/OasisLib/src/dk/aau/cs/giraf/oasis/lib/metadata/AuthUsersMetaData.java
+++ branches/Group_4/OasisLib/src/dk/aau/cs/giraf/oasis/lib/metadata/AuthUsersMetaData.java
@@ -3,14 +3,14 @@ package dk.aau.cs.giraf.oasis.lib.metadata;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
-public class CertificatesMetaData {
+public class AuthUsersMetaData {
 
-	public static final Uri CONTENT_URI = Uri.parse("content://sw6.oasis.provider.AutismProvider/certificates");
+	public static final Uri CONTENT_URI = Uri.parse("content://dk.aau.cs.giraf.oasis.localdb.AutismProvider/certificates");
 
 	public class Table implements BaseColumns {
 		public static final String TABLE_NAME = "tbl_certificates";
 
 		public static final String COLUMN_ID = "_id";
-		public static final String COLUMN_AUTHKEY = "certificates_authkey";
+		public static final String COLUMN_CERTIFICATE = "authusers_certificate";
 	}
 }
