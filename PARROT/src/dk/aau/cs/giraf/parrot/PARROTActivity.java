8544934517eff3c767e36199b5f455567009561f
rasmusdalhoffjensen@hotmail.com <rasmusdalhoffjensen@hotmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-04 08:31:16 +0000

    Developer Name: R. D-J Synopsis: Added older version

--- PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
+++ PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
@@ -16,28 +16,9 @@ public class PARROTActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
  	    
-        Category temp1 = new Category(0xff0000); //TODO dummy variable. Change this when database is up.
-        Pictogram pic = new Pictogram("001", "/root/pictures/001.jpg", null, null);
-        temp1.addPictogram(pic);
-        pic = new Pictogram("002", "/root/pictures/002.jpg", null, null);
-        temp1.addPictogram(pic);
-        pic = new Pictogram("003", "/root/pictures/003.jpg", null, null);
-        temp1.addPictogram(pic);
-        pic = new Pictogram("004", "/root/pictures/004.jpg", null, null);
-        temp1.addPictogram(pic);
-        pic = new Pictogram("005", "/root/pictures/005.jpg", null, null);
-        temp1.addPictogram(pic);
-        pic = new Pictogram("006", "/root/pictures/006.jpg", null, null);
-        temp1.addPictogram(pic);
-        pic = new Pictogram("007", "/root/pictures/007.jpg", null, null);
-        temp1.addPictogram(pic);
-        pic = new Pictogram("008", "/root/pictures/008.jpg", null, null);
-        temp1.addPictogram(pic);
-         
-        GridView superCategory = (GridView) findViewById(R.id.supercategory); //This is to show the supercategories
-		superCategory.setAdapter(new PictogramAdapter(temp1, this)); //FIXME
+
         
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
--- PARROT/src/dk/aau/cs/giraf/parrot/SpeechBoardFragment.java
+++ PARROT/src/dk/aau/cs/giraf/parrot/SpeechBoardFragment.java
@@ -1,17 +1,36 @@
 package dk.aau.cs.giraf.parrot;
 
+
 import parrot.Package.R;
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnAttachStateChangeListener;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 public class SpeechBoardFragment extends Fragment
 {
+	public SpeechBoardFragment()
+	{
+		
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+		
+	}
+	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) 
-	{
+	{        
+		
+
+
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.speechboard_layout, container, false);
 	}
