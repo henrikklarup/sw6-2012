bfa2c6060c5e2acfbff57eb72de598c087ab9587
mfjordvald@gmail.com <mfjordvald@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-07 09:49:00 +0000

    Updated EventHandler

--- savannah/src/savannah/test/EventHandlerTest.java
+++ savannah/src/savannah/test/EventHandlerTest.java
@@ -1,5 +1,31 @@
 package savannah.test;
 
+import java.awt.EventQueue;
+import java.io.File;
+import java.util.ArrayList;
+
+import org.jdom.Document;
+
+import savannah.io.DOMinator.DOMinator;
+import savannah.server.CommitEvent;
+import savannah.server.EventHandler;
+
 public class EventHandlerTest {
-		//TODO
+		private static EventHandler handler = new EventHandler();
+		
+		public static void main(String[] args) throws Exception
+		{
+			File f = new File("/home/martin/Documents/eadocs/sw6_example.xml");
+			DOMinator dom = new DOMinator();
+			Document d = dom.Dominate(f);
+			
+			CommitEvent com = new CommitEvent(d,null,false);
+			handler.CommitHandler(com);
+			ArrayList<String> q = handler.getQueries();
+			for(String s: q)
+			{
+				System.out.println(s);
+			}
+			System.out.println("done");
+		}
 }
