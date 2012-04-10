cfc832ec21fc57a0651c81439ed7141b3800fb86
mfjordvald@gmail.com <mfjordvald@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-10 14:11:54 +0000

    UPDATES! meijn!

--- savannah/src/savannah/server/CommitHandler.java
+++ savannah/src/savannah/server/CommitHandler.java
@@ -8,19 +8,38 @@ import org.jdom.JDOMException;
 public class CommitHandler {
 	private ArrayList<String> queries;
 	private QueryBuilder qbuilder;
+	private QueryHandler qHandler;
 	public CommitHandler()
 	{
 		qbuilder = new QueryBuilder();
 		queries= new ArrayList<String>();
+		qHandler = new QueryHandler();
 	}
 	
 	public void HandleIt(CommitEvent e)
 	{
 	try{
 			Document xml = e.getEventContent();
+		
 			queries = qbuilder.buildQueries(xml);
 			
 			//TODO Run all queries and respond to sender, success or not
+			int failed = 0;
+			
+			for(String s: queries)
+			{
+				 int result = qHandler.SendQuery(s);
+				 if(result == 0)
+				 {
+					 failed++;
+				 }
+			}
+			
+			//TODO Send result
+			
+			failed = 0;
+				
+			
 		}
 	catch (JDOMException f)
 		{ f.printStackTrace(); }
