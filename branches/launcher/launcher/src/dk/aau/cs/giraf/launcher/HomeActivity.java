987b58b4094abe7e123d4e6ce07341593eeeaf43
call911c@gmail.com <call911c@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-11 13:06:32 +0000

    Developer Name: Steiniche Synopsis: Project merged

---
+++ branches/launcher/launcher/src/dk/aau/cs/giraf/launcher/HomeActivity.java
@@ -0,0 +1,65 @@
+package dk.aau.cs.giraf.launcher;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.GridView;
+
+public class HomeActivity extends Activity {
+
+	private static Context context;
+	private static ArrayList<ApplicationInfo> mApplications;
+	GridView Grid;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		loadApplications(true);
+		
+	}
+	private void loadApplications(boolean isLaunching) {
+		if (isLaunching && mApplications != null) {
+			return;
+		}
+		
+		Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		
+		HomeActivity.context = getApplicationContext();
+		final List<ResolveInfo> pkgAppsList = context.getPackageManager().queryIntentActivities( mainIntent, 0);
+
+		if(pkgAppsList != null){
+			ArrayList<ApplicationInfo> applications = new ArrayList<ApplicationInfo>();
+			for(ResolveInfo info : pkgAppsList){
+				//Package (dk.aau.cs.giraf)
+				if(!info.toString().toLowerCase().contains("dk.aau.cs.giraf") && !info.toString().toLowerCase().contains("launcher")){
+					ApplicationInfo appInfo = new ApplicationInfo();
+					appInfo.title = info.loadLabel(getPackageManager());
+					appInfo.icon = info.activityInfo.loadIcon(getPackageManager());
+
+					appInfo.setActivity(new ComponentName(
+	                        info.activityInfo.applicationInfo.packageName,
+	                        info.activityInfo.name),
+	                        Intent.FLAG_ACTIVITY_NEW_TASK
+	                        | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+					applications.add(appInfo);
+				}
+			}
+
+			Grid = (GridView)findViewById(R.id.GridViewHome);
+			Grid.setAdapter(new AppAdapter(this,applications));
+			Grid.setOnItemClickListener(new ApplicationLauncher());
+		}
+	}
+}
