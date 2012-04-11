694e131fe5082af32a1e2eb36c3b2501ba778be8
thorkvist@gmail.com <thorkvist@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-11 11:48:38 +0000

    Developer Name: Thorbjørn Kvist Nielsen Synopsis: Made the IOHandler singleTON and fixed the ripple-effects

--- savannah/src/savannah/serverMain/ServerMain.java
+++ savannah/src/savannah/serverMain/ServerMain.java
@@ -7,7 +7,7 @@ public class ServerMain {
 	
 	public static void main(String[] args) {
 		new EventHandler().run();
-		new IOHandler(ServerConfiguration.PORT, ServerConfiguration.FOLDERPATH, ServerConfiguration.BUFFERSIZE);
+		IOHandler.getInstance().run();
 	}
 
 }
