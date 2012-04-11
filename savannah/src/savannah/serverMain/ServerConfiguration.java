f4874caec065b369c0a3dd7579fac11e231b420e
thorkvist@gmail.com <thorkvist@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-11 11:19:07 +0000

    Developer Name: Thorbjørn Kvist Nielsen Synopsis: Updating Server initiation routine and adding classes for a device connection.

--- savannah/src/savannah/serverMain/ServerConfiguration.java
+++ savannah/src/savannah/serverMain/ServerConfiguration.java
@@ -1,6 +1,6 @@
 package savannah.serverMain;
 
-public class ServerConfiguration {
+final class ServerConfiguration {
 	public static final int PORT			= 50000;
 	public static final int BUFFERSIZE		= 4096;
 	public static final String FOLDERPATH	= "C:\\newFolder";
