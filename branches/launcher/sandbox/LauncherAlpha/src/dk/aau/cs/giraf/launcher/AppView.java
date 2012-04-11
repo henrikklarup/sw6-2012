bd44f2a7d88b5fc76c1070495656da2e22880bb8
call911c@gmail.com <call911c@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-11 10:43:43 +0000

    Developer Name: Rasmus Steiniche Synopsis: background color

---
+++ branches/launcher/sandbox/LauncherAlpha/src/dk/aau/cs/giraf/launcher/AppView.java
@@ -0,0 +1,19 @@
+package dk.aau.cs.giraf.launcher;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+public class AppView extends ViewGroup{
+
+	public AppView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
