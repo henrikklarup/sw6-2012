7b52b8729c3c290f270a2e3a5550b220be37ff4d
kas.m.andersen@gmail.com <kas.m.andersen@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-11 07:50:49 +0000

    Developer Name: Synopsis:

---
+++ branches/launcher/sandbox/LauncherAlpha/src/dk/aau/cs/giraf/launcher/AppAdapter.java
@@ -0,0 +1,48 @@
+package dk.aau.cs.giraf.launcher;
+
+import giraffe.launcher.R;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AppAdapter extends ArrayAdapter<ApplicationInfo> {
+
+	Context context;
+	ArrayList<ApplicationInfo> apps;
+	
+	public AppAdapter(Context context, ArrayList<ApplicationInfo> apps) {
+		super(context, 0, apps);
+		
+		this.context = context;
+		this.apps = apps;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		final ApplicationInfo app = apps.get(position);
+		
+		if (convertView == null) {
+            final LayoutInflater inflater = 
+            		(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.grid, parent, false);
+        }
+		
+		ImageView iv = (ImageView) convertView.findViewById(R.id.app_icon);
+		TextView tv = (TextView) convertView.findViewById(R.id.app_text);
+
+        tv.setText(app.title.toString());
+        iv.setBackgroundDrawable(app.icon);
+
+        return convertView;
+		
+	}
+	
+}
--- branches/launcher/sandbox/LauncherAlpha/src/dk/aau/cs/giraf/launcher/ApplicationLaunch.java
+++ branches/launcher/sandbox/LauncherAlpha/src/dk/aau/cs/giraf/launcher/ApplicationLaunch.java
@@ -1,23 +1,14 @@
 package dk.aau.cs.giraf.launcher;
 
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Toast;
 import android.app.Activity;
-import android.content.Intent;
 
 	class ApplicationLaunch extends Activity implements AdapterView.OnItemClickListener{
         @Override
         public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
             ApplicationInfo app = (ApplicationInfo) parent.getItemAtPosition(position);
-			try {
-				Log.d("GIRAF", ""+app.intent.getData());
-				startActivity(app.intent);
-			}
-			catch(NullPointerException e){
-				Log.i("GIRAF", " " +e.getMessage());	
-			}
+            startActivity(app.intent);
     }
         
 }
