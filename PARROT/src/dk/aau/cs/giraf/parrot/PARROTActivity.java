f5ca53cd66e6b0305610f21d56a7d0338cf22a56
rasmusdalhoffjensen@hotmail.com <rasmusdalhoffjensen@hotmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-11 10:27:39 +0000

    Developer Name: Rasmus D-J Synopsis: Started implementing a loadProfile function in PARROTActivity.java.

--- PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
+++ PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
@@ -2,6 +2,9 @@ package dk.aau.cs.giraf.parrot;
 
 
 
+import dk.aau.cs.giraf.oasis.lib.Helper;
+import dk.aau.cs.giraf.oasis.lib.models.Media;
+import dk.aau.cs.giraf.oasis.lib.models.Profile;
 import parrot.Package.R;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
@@ -16,10 +19,12 @@ public class PARROTActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
- 	    /*
-
-        */
+        
+        PARROTProfile parrotUser = loadProfile();
+        
+       
+        
+        
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         actionBar.setDisplayShowTitleEnabled(false);	//TODO figure out what this does
@@ -34,5 +39,36 @@ public class PARROTActivity extends Activity {
         		.setTabListener(new TabListener<OptionsFragment>(this,"options",OptionsFragment.class));
         actionBar.addTab(tab);
        
+    }
+    
+    public PARROTProfile loadProfile()
+    {
+    	 //This part of the code is supposed to get a profile from the launcher, and read it from the admin.
+        Bundle extras = getIntent().getExtras();
+        Profile prof;
+        Helper help = new Helper(this);
+        if(extras !=null)
+        {
+        	prof = help.profilesHelper.getProfileById(extras.getLong("currentProfileId"));
+        	Pictogram pic = new Pictogram(prof.getFirstname(), prof.getPicture(), null, null);
+        	PARROTProfile parrotUser = new PARROTProfile(prof.getFirstname(), pic);
+        	
+        	//TODO read categories from settings
+        	//TODO load medias into pictogram categories using settings.
+        	return parrotUser;
+        }
+        else
+        {
+        	//If no profile is found, return null.
+        	//TODO find out if this means that a Guardian is using the PARROT app.
+        	return null;
+        }
+    	
+    }
+    
+    public void saveProfile(PARROTProfile user)
+    {
+    	//TODO write this method so that it sends the changes to the admin.
+    	
     }
 }
