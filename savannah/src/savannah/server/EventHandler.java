026ecc87ae06256e57bc75aa4d69a9040721c67c
mfjordvald@gmail.com <mfjordvald@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-11 11:16:12 +0000

    Another savannah server update

--- savannah/src/savannah/server/EventHandler.java
+++ savannah/src/savannah/server/EventHandler.java
@@ -5,12 +5,27 @@ import java.sql.SQLException;
 public class EventHandler implements Runnable {
 	private CommitHandler cHandler;
 	private RequestHandler rHandler;
-	public EventHandler() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
+	public EventHandler()
 	{
-		cHandler = new CommitHandler();
+		try {
+			cHandler = new CommitHandler();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		rHandler = new RequestHandler();
 	}
 	public void start() {
+		
 		EventQueue eque = EventQueue.getInstance();
 		while(eque.isEmpty()) {
 			while(!eque.isEmpty()) {
@@ -25,6 +40,7 @@ public class EventHandler implements Runnable {
 				}
 			}
 		}
+		
 	}
 
 
