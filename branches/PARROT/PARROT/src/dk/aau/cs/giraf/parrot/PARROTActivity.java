160f209c88e8d243baa9c088cd387e6c1927c9e8
rasmusdalhoffjensen@hotmail.com <rasmusdalhoffjensen@hotmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-11 12:37:43 +0000

    Developer Name: Rasmus D-J Synopsis: Kobling til database.

--- branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
+++ branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
@@ -2,9 +2,12 @@ package dk.aau.cs.giraf.parrot;
 
 
 
+import java.util.List;
+
 import dk.aau.cs.giraf.oasis.lib.Helper;
 import dk.aau.cs.giraf.oasis.lib.models.Media;
 import dk.aau.cs.giraf.oasis.lib.models.Profile;
+import dk.aau.cs.giraf.oasis.lib.models.Setting;
 import parrot.Package.R;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
@@ -15,60 +18,131 @@ import android.widget.GridView;
 
 
 public class PARROTActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        PARROTProfile parrotUser = loadProfile();
-        
-       
-        
-        
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionBar.setDisplayShowTitleEnabled(false);	//TODO figure out what this does
-        
-        Tab tab = actionBar.newTab()
-        		.setText(R.string.firstTab)		//TODO rename this tab
-        		.setTabListener(new TabListener<SpeechBoardFragment>(this,"speechboard",SpeechBoardFragment.class));
-        actionBar.addTab(tab);
-        
-        tab = actionBar.newTab()
-        		.setText(R.string.secondTab)		//TODO rename this tab
-        		.setTabListener(new TabListener<OptionsFragment>(this,"options",OptionsFragment.class));
-        actionBar.addTab(tab);
-       
-    }
-    
-    public PARROTProfile loadProfile()
-    {
-    	 //This part of the code is supposed to get a profile from the launcher, and read it from the admin.
-        Bundle extras = getIntent().getExtras();
-        Profile prof;
-        Helper help = new Helper(this);
-        if(extras !=null)
-        {
-        	prof = help.profilesHelper.getProfileById(extras.getLong("currentProfileId"));
-        	Pictogram pic = new Pictogram(prof.getFirstname(), prof.getPicture(), null, null);
-        	PARROTProfile parrotUser = new PARROTProfile(prof.getFirstname(), pic);
-        	
-        	//TODO read categories from settings
-        	//TODO load medias into pictogram categories using settings.
-        	return parrotUser;
-        }
-        else
-        {
-        	//If no profile is found, return null.
-        	//TODO find out if this means that a Guardian is using the PARROT app.
-        	return null;
-        }
-    	
-    }
-    
-    public void saveProfile(PARROTProfile user)
-    {
-    	//TODO write this method so that it sends the changes to the admin.
-    	
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		PARROTProfile parrotUser = loadProfile();
+
+
+
+
+		ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.setDisplayShowTitleEnabled(false);	//TODO figure out what this does
+
+		Tab tab = actionBar.newTab()
+				.setText(R.string.firstTab)		//TODO rename this tab
+				.setTabListener(new TabListener<SpeechBoardFragment>(this,"speechboard",SpeechBoardFragment.class));
+		actionBar.addTab(tab);
+
+		tab = actionBar.newTab()
+				.setText(R.string.secondTab)		//TODO rename this tab
+				.setTabListener(new TabListener<OptionsFragment>(this,"options",OptionsFragment.class));
+		actionBar.addTab(tab);
+
+	}
+
+	public PARROTProfile loadProfile()
+	{
+		//This part of the code is supposed to get a profile from the launcher, and read it from the admin.
+		Bundle extras = getIntent().getExtras();
+		Profile prof;
+		Helper help = new Helper(this);
+		if(extras !=null)
+		{
+			prof = help.profilesHelper.getProfileById(extras.getLong("currentProfileId"));
+			Pictogram pic = new Pictogram(prof.getFirstname(), prof.getPicture(), null, null);
+			PARROTProfile parrotUser = new PARROTProfile(prof.getFirstname(), pic);
+
+			//TODO read categories from settings
+			Setting<String, String, String> specialSettings = null;
+
+			//TODO load medias into pictogram categories using settings.
+
+			//Add all of the categories to the profile
+			int number = 0;
+			String categoryString=null;
+			while (true)
+			{
+				categoryString = specialSettings.get(prof.getFirstname()).get("category"+number);
+				if(categoryString !=null)
+				{
+					String colourString = specialSettings.get(prof.getFirstname()).get("category"+number+"colour");
+					int col=Integer.valueOf(colourString);
+					//If the category of that number exists
+					parrotUser.addCategory(loadCategory(categoryString,col,help));
+				}
+				else
+				{
+					break;
+				}
+
+			}
+
+			return parrotUser;
+		}
+		else
+		{
+			//If no profile is found, return null.
+			//TODO find out if this means that a Guardian is using the PARROT app.
+			return null;
+		}
+
+	}
+
+	public Category loadCategory(String pictureIDs,int colour,Helper help)
+	{
+		Category cat = new Category(colour);
+		List<Integer> listIDs = getIDsFromString(pictureIDs);
+		for(int i = 0; i<listIDs.size();i++)
+		{
+			cat.addPictogram(loadPictogram(listIDs.get(i),help));
+		}
+		return cat;
+	}
+
+	public Pictogram loadPictogram(int id,Helper help)
+	{
+		Pictogram pic = null;
+		Media media=help.mediaHelper.getSingleMediaById(id);
+		//TODO figure out how the Media class is organised, and how I can find the sound, word and picture path.
+		return pic;
+	}
+
+	//This function takes a string consisting of IDs, and returns a list of integer IDs instead
+	public List<Integer> getIDsFromString(String IDstring)
+	{
+		List<Integer> listOfID = null;
+		
+		if(IDstring !=null || IDstring.charAt(0)!='$'||IDstring.charAt(0)!='#')
+		{
+			String temp = String.valueOf(IDstring.charAt(0));
+			int w = 0;
+			while(IDstring.charAt(w)!='$')
+			{
+				w++;
+				if(IDstring.charAt(w)!='#')
+				{
+					temp = temp+ IDstring.charAt(w);
+				}
+				else
+				{
+					listOfID.add(Integer.valueOf(temp));
+					w++;
+					temp = String.valueOf(IDstring.charAt(w));
+				}
+
+			}
+		}
+
+		return listOfID;
+	}
+
+	public void saveProfile(PARROTProfile user)
+	{
+		//TODO write this method so that it sends the changes to the admin.
+
+	}
 }
