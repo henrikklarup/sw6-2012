1320405890fa9bfef989d68d9ddf3cb9de9dc5e9
call911c@gmail.com <call911c@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-11 10:43:43 +0000

    Developer Name: Rasmus Steiniche Synopsis: background color

--- sandbox/LauncherAlpha/src/dk/aau/cs/giraf/launcher/AppAdapter.java
+++ sandbox/LauncherAlpha/src/dk/aau/cs/giraf/launcher/AppAdapter.java
@@ -1,15 +1,19 @@
 package dk.aau.cs.giraf.launcher;
 
 import giraffe.launcher.R;
+import giraffe.launcher.R.drawable;
 
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.graphics.drawable.shapes.Shape;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class AppAdapter extends ArrayAdapter<ApplicationInfo> {
@@ -31,17 +35,17 @@ public class AppAdapter extends ArrayAdapter<ApplicationInfo> {
 		
 		if (convertView == null) {
             final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.grid, parent, false);
+            convertView = inflater.inflate(R.layout.apps, parent, false);
         }
-		
+		LinearLayout ll = (LinearLayout) convertView.findViewById(R.id.app_bg);
 		ImageView iv = (ImageView) convertView.findViewById(R.id.app_icon);
 		TextView tv = (TextView) convertView.findViewById(R.id.app_text);
-
+		
         tv.setText(app.title.toString());
-        iv.setBackgroundDrawable(app.icon);
+        iv.setImageDrawable(app.icon);
+        
 
         return convertView;
-		
 	}
 	
 }
