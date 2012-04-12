787df76b4a47392d852688ebbbc6d8c709695bd2
rasmus5034@gmail.com <rasmus5034@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-12 14:15:15 +0000

    Developer Name: Rasmus Aaen Synopsis: Going for 1337

--- Wombat/src/dk/aau/cs/giraf/wombat/SubProfileFragment.java
+++ Wombat/src/dk/aau/cs/giraf/wombat/SubProfileFragment.java
@@ -74,7 +74,7 @@ public class SubProfileFragment extends android.app.ListFragment {
 		SubProfileAdapter adapter = new SubProfileAdapter(getActivity(),
 				android.R.layout.simple_list_item_1, subprofiles);
 		setListAdapter(adapter);  
-	}
+	} 
 
 	public void onListItemClick(ListView lv, View view, int position, long id) {
 		if(TimerLoader.firstClick){
