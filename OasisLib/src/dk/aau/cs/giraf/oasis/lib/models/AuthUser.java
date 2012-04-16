e05cd380006f5998d0ef6a02380b23811b3abd88
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-16 08:45:32 +0000

    Developer Name: Dan Møller Synopsis: Added AuthUser and AuthUsersHelper

---
+++ OasisLib/src/dk/aau/cs/giraf/oasis/lib/models/AuthUser.java
@@ -0,0 +1,70 @@
+package dk.aau.cs.giraf.oasis.lib.models;
+
+public class AuthUser {
+
+	private long id;
+	private String certificate;
+	private long aRole;
+	private static String _output = "{0}, {1}, {2}";
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the certificate
+	 */
+	public String getCertificate() {
+		return certificate;
+	}
+	/**
+	 * @param certificate the certificate to set
+	 */
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+	/**
+	 * @return the aRole
+	 */
+	public long getaRole() {
+		return aRole;
+	}
+	/**
+	 * @param aRole the aRole to set
+	 */
+	public void setaRole(long aRole) {
+		this.aRole = aRole;
+	}
+
+	/**
+	 * Set output
+	 * {0} = Id
+	 * {1} = Certificate
+	 * {2} = Role
+	 * @param output the output to set
+	 */
+	public static void setOutput(String output) {
+		_output = output;
+	}
+
+	/**
+	 * toString for authuser
+	 */
+	@Override
+	public String toString() {
+
+		String localOutput = _output;
+		localOutput = localOutput.replace("{0}", String.valueOf(getId()));
+		localOutput = localOutput.replace("{1}", getCertificate());
+		localOutput = localOutput.replace("{2}", String.valueOf(getaRole()));
+
+		return localOutput;
+	}
+}
