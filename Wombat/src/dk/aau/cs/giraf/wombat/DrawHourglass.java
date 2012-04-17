103964822f6dff638be16fcb45bc0405c3bde9ca
sbf.aau@gmail.com <sbf.aau@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-17 12:15:29 +0000

    Developer Name: simon bf Synopsis: begyndt på timeglasset

--- Wombat/src/dk/aau/cs/giraf/wombat/DrawHourglass.java
+++ Wombat/src/dk/aau/cs/giraf/wombat/DrawHourglass.java
@@ -14,22 +14,21 @@ public class DrawHourglass extends View {
 	private Guardian guard = Guardian.getInstance();
 	SubProfile sp = guard.getSubProfile();
 	int background = sp._bgcolor;
-	int frame = sp._frameColor;
-	int timeleft = sp._timeLeftColor;
-	int timeleft2 = sp._timeLeftColor;
-	int timespent = sp._timeSpentColor;
+	int frame = sp.frameColor;
+	int timeleft = sp.timeLeftColor;
+	int timeleft2 = sp.timeLeftColor;
+	int timespent = sp.timeSpentColor;
 	boolean init = true;
 
 	double totalTime = (sp._totalTime - 1) * 1000;
 	double endTime = SystemClock.currentThreadTimeMillis() + totalTime;
 	Paint paint = new Paint();
-	Rect r;
+	Rect rtop, rbot, rleft, rright;
 	ColorDrawable col;
 
-	int width;
-	int height;
-	int left;
-	int top;
+	int width, height;
+	int top, left;
+	int frameHeight, indent;
 
 	public DrawHourglass(Context context) {
 		super(context);
@@ -44,57 +43,72 @@ public class DrawHourglass extends View {
 		super.onDraw(c);
 
 		if(init){
-			width = (c.getWidth()/8)*5;
-			height = (int) (width*0.2);
+			width = 300;
+			height = 500;
+			frameHeight = height / 20;
+			indent = (int) (frameHeight * 1.5);
 			init = false;
 		}
 		/* Fill the canvas with the background color */
 		paint.setColor(background);
 		c.drawPaint(paint);
 
-		/* Draw the frame of the progressbar */
+		/* Draw the frame of the hourglass */
 		paint.setAntiAlias(true);
 		paint.setColor(frame);
 		
 		left = (c.getWidth() - width) / 2;
 		top = (c.getHeight() - height) / 2;
 
-		r = new Rect(left, top, left + width, top + height);
-		c.drawRect(r, paint);
+		rtop = new Rect(left, top, left + width, top + frameHeight);
+		rbot = new Rect(left, top + height - frameHeight, left + width, top + height);
+		rleft = new Rect(left + indent, top, left - frameHeight + indent, top + height);
+		rright = new Rect(left + width - indent, top, left + width - indent + frameHeight, top + height);
+		
+		c.drawRect(rtop, paint);
+		c.drawRect(rbot, paint);
+		c.drawRect(rleft, paint);
+		c.drawRect(rright, paint);
+		
+		/* Draw the "glass" */
+		
+		
 
 		/* Draw the backgroundcolor inside the frame */
-		paint.setColor(background);
-		r.set(r.left + 2, r.top + 2, r.right - 2, r.bottom - 2);
-		c.drawRect(r, paint);
+//		paint.setColor(background);
+//		rtop.set(r.left + 2, r.top + 2, r.right - 2, r.bottom - 2);
+//		c.drawRect(r, paint);
 
-		/* Draw the timespent color (on the right) on top of the timeleft */
-		paint.setColor(timespent);
-		r.set(left + 3, top + 3, left + width - 3, top + height - 3);
-		c.drawRect(r, paint);
-
-		if (endTime >= SystemClock.currentThreadTimeMillis()) {
-			paint.setStyle(Paint.Style.FILL);
-			double percent = (endTime - SystemClock.currentThreadTimeMillis())
-					/ totalTime;
-
-			paint.setColor(timeleft2);
-			r.set((left + 3), top + 3, left + 3 + (int) (width * percent), top + height - 3);
-			c.drawRect(r, paint);
-			
-			/* Draw the timeleft color (on the left side) */
-			col = new ColorDrawable(timeleft);
-			col.setAlpha((int) (255*percent));
-			paint.setColor(col.getColor());
-			r.set((left + 3), top + 3, left + 3 + (int) (width * percent), top + height - 3);
-			c.drawRect(r, paint);
-			
-			/*************** IMPORTANT ***************/
-			/* Recalls Draw! */
-			invalidate();
-		} else {
-			paint.setColor(timespent);
-			r.set(left + 3, top + 3, left + width - 3, top + height - 3);
-			c.drawRect(r, paint);
+//		/* Draw the timespent color (on the right) on top of the timeleft */
+//		paint.setColor(timespent);
+//		r.set(left + 3, top + 3, left + width - 3, top + height - 3);
+//		c.drawRect(r, paint);
+//
+//		if (endTime >= SystemClock.currentThreadTimeMillis()) {
+//			paint.setStyle(Paint.Style.FILL);
+//			double percent = (endTime - SystemClock.currentThreadTimeMillis())
+//					/ totalTime;
+//
+//			paint.setColor(timeleft2);
+//			r.set((left + 3), top + 3, left + 3 + (int) (width * percent), top + height - 3);
+//			c.drawRect(r, paint);
+//			
+//			/* Draw the timeleft color (on the left side) */
+//			col = new ColorDrawable(timeleft);
+//			col.setAlpha((int) (255*percent));
+//			paint.setColor(col.getColor());
+//			r.set((left + 3), top + 3, left + 3 + (int) (width * percent), top + height - 3);
+//			c.drawRect(r, paint);
+//			
+//			/*************** IMPORTANT ***************/
+//			/* Recalls Draw! */
+//			invalidate();
+//		} else {
+//			paint.setColor(timespent);
+//			r.set(left + 3, top + 3, left + width - 3, top + height - 3);
+//			rb.set(left + 3, top + 3, left + width - 3, top + height -3);
+//			c.drawRect(r, paint);
+//			c.drawRect(rb, paint);
 		}
 	}
-}
+
