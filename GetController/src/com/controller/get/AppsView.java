742900b6a39435e68a816e43816a075cbc0bc67f
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-17 11:58:02 +0000

    Developer Name: Dan Møller Synopsis: GetController test of settings

--- GetController/src/com/controller/get/AppsView.java
+++ GetController/src/com/controller/get/AppsView.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -29,102 +30,89 @@ public class AppsView extends ListActivity {
 	int _position;
 	List<App> values;
 	List<Profile> profileValues;
+	Long testId;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		helper = new Helper(this);
-		settings = new Setting<String, String, String>();
-		settings.addValue("Hello", "String", "World!");
-
+		
+		
 		//profile er en forud deklareret profil
 		Bundle extras = getIntent().getExtras();
-		        if (extras != null) {
-		            profile = helper.profilesHelper.getProfileById(extras.getLong("ID"));
-		        } else {
-		        	profile = new Profile("Dummy1", "Dummy","Dummy", 0, 12345678, "Dummy", settings);
-		        }
-//		profile = new Profile("Dummy1", "Dummy","Dummy", 0, 12345678, "Dummy", settings);
-//		int result = helper.profilesHelper.insertProfile(profile);
+		if (extras != null) {
+			profile = helper.profilesHelper.getProfileById(extras.getLong("ID"));
+			settings = new Setting<String, String, String>();
+			settings.addValue(Long.toString(profile.getId()), "Settings", "Favorite Color, Favorite Food, Favorite Animal");
+			settings.addValue(Long.toString(profile.getId()), "Favorite Color", "Blue");
+			settings.addValue(Long.toString(profile.getId()), "Favorite Food", "Carrot");
+			settings.addValue(Long.toString(profile.getId()), "Favorite Animal", "Rabbit");
+			profile.setSetting(settings);
+		} else {
+			profile = new Profile("Dummy1", "Dummy","Dummy", 0, 12345678, "Dummy", settings);
+		}
+
 		
-//		if (result == 0) {
-//			profileValues.add(helper.profilesHelper.authenticateProfile("Certificate"));
-//		}
-//		
-//		Profile profile1 = new Profile("Dummy1", "Dummy","Dummy", 0, 12345678, "Dummy", new Setting<String,String,String>());
-//		profile1.setId(504);
-//		
-//		profileValues.add(profile1);
-	
-//		
-//		app = new App();
-//		app.setName("GetControllerInsert");
-//		app.setVersion("versionNumber");
-//		helper.appsHelper.insertApp(app);
-//		helper.appsHelper.insertApp(app);
-//		helper.appsHelper.insertApp(app);
-//
-//		values = helper.appsHelper.getApps();
+
 
 		updateList();
 
 		bAdd = (Button) findViewById(R.id.add);
 		bAdd.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-//				helper.appsHelper.insertApp(app);
+				// helper.appsHelper.insertApp(app);
 				helper.profilesHelper.insertProfile(profile);
 				updateList();
 			}
 		});
-		
+
 		bDel = (Button) findViewById(R.id.delete);
 		bDel.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-//				helper.appsHelper.clearAppsTable();
+				// helper.appsHelper.clearAppsTable();
 				helper.profilesHelper.clearProfilesTable();
 				updateList();
 			}
 		});
-		
+
 	}
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 
-//		App _app = new App();
-//		App fedte = new App();
-//		_app = (App) getListAdapter().getItem(position);
-//		long _id = _app.getId();
-//
-//		fedte = helper.appsHelper.getAppById(_id);
-//
 		Profile fedte = new Profile();
 		fedte = helper.profilesHelper.getProfileById(((Profile)getListAdapter().getItem(position)).getId());
-		
+//		fedte = helper.profilesHelper.getProfileById(testId);
+
 		if (fedte != null) {
-			Toast.makeText(this, fedte.getSetting().get("Hello").get("String"), Toast.LENGTH_SHORT).show();
+			Log.e("Profile ID", Long.toString(((Profile)getListAdapter().getItem(position)).getId()));
+			String settings = fedte.getSetting().get(Long.toString(testId)).get("Settings");
+			String[] settingsList = settings.split(",");
+			for (String setting : settingsList) {
+				Toast.makeText(this, fedte.getSetting().get(Long.toString(testId)).get(setting), Toast.LENGTH_SHORT).show();
+			}
 		} else {
 			//   Toast.makeText(this, String.valueOf(_id), Toast.LENGTH_SHORT).show();
 		}
-		
+
 		updateList();
 	}
-	
+
 	public void updateList() {
 		profileValues = helper.profilesHelper.getProfiles();
 		profileAdapter = new ArrayAdapter<Profile>(this, android.R.layout.simple_list_item_1, profileValues);
 		setListAdapter(profileAdapter);
-		
-//		values = helper.appsHelper.getApps();
-//		adapter = new ArrayAdapter<App>(this, android.R.layout.simple_list_item_1, values);
-//		setListAdapter(adapter);
+
+		//		values = helper.appsHelper.getApps();
+		//		adapter = new ArrayAdapter<App>(this, android.R.layout.simple_list_item_1, values);
+		//		setListAdapter(adapter);
 	}
 }
