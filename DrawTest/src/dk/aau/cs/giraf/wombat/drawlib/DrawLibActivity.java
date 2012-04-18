2f170ec0b25ab05b6cffd950014911f78e072b50
rasmus5034@gmail.com <rasmus5034@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-18 06:58:20 +0000

    Developer Name: Rasmus Aaen Synopsis: Updated wombat drawlib

---
+++ DrawTest/src/dk/aau/cs/giraf/wombat/drawlib/DrawLibActivity.java
@@ -0,0 +1,52 @@
+package dk.aau.cs.giraf.wombat.drawlib;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.LinearLayout;
+import dk.aau.cs.giraf.TimerLib.ProgressBar;
+import dk.aau.cs.giraf.TimerLib.SubProfile;
+
+
+public class DrawLibActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		SubProfile sub = new ProgressBar("", "", 100, 0xff3D3D3D, 0xff000066, 0xffB8B8B8, 0xff000000, 30, true);
+		if (sub.getAttachment() == null) {
+			/* Set the drawing class (which extends View) as the content view */
+			View v = genDrawView(sub);
+			setContentView(v);
+		} else {
+			// Get display size
+			Display disp = getWindowManager().getDefaultDisplay();
+			Point size = new Point();
+			disp.getSize(size);
+			
+			LinearLayout frame = new LinearLayout(this);
+			View v = genDrawView(sub);
+			v.setTag("main");
+			frame.addView(v, size.x/2, size.y);
+			
+			v = genDrawView(sub.getAttachment());
+			v.setTag("attachment");
+			frame.addView(v, size.x/2, size.y);
+			
+			setContentView(frame);
+		}
+
+	}
+
+	private View genDrawView(SubProfile sub) {
+		switch (sub.formType()) {
+		case ProgressBar:
+			return new DrawProgressBar(getApplicationContext(), sub);
+		default:
+			return null;
+		}
+	}
+}
