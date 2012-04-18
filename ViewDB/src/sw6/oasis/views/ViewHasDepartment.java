034e7e1c74b1a5f8b8f20a333aa0c32defae61d5
jensmohr90@gmail.com <jensmohr90@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-18 21:06:28 +0000

    small update af viewdb

---
+++ ViewDB/src/sw6/oasis/views/ViewHasDepartment.java
@@ -0,0 +1,78 @@
+package sw6.oasis.views;
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import dk.aau.cs.giraf.oasis.lib.Helper;
+import dk.aau.cs.giraf.oasis.lib.models.HasDepartment;
+
+public class ViewHasDepartment extends ListFragment {
+
+	Helper helper;
+	Button bAdd, bDel;
+	TextView tvHeader;
+	ArrayAdapter<HasDepartment> mAdapter;
+	List<HasDepartment> valueList;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.tableview, container, false);
+		
+		return view;
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		helper = new Helper(getActivity().getApplicationContext());
+		
+		tvHeader = (TextView) getView().findViewById(R.id.table_header);
+		tvHeader.setText("HasDepartmentTable");
+		bAdd = (Button) getView().findViewById(R.id.add);
+		bAdd.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getActivity().getApplicationContext(), "ADD", Toast.LENGTH_SHORT).show();
+			}
+		});
+		bDel = (Button) getView().findViewById(R.id.delete);
+		bDel.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getActivity().getApplicationContext(), "CLEAR", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		valueList = helper.hdc.getHasDepartments();
+
+        mAdapter = new ArrayAdapter<HasDepartment>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+
+        setListAdapter(mAdapter);
+
+	}
+	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		long _id = getListAdapter().getItemId(position);
+		Toast.makeText(getActivity().getApplicationContext(), "LIST ITEM CLICK", Toast.LENGTH_SHORT).show();
+	}
+}
