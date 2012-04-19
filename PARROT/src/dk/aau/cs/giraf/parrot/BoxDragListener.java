0df39f411ad5005833d8e568015b90aa5378f08e
rasmusdalhoffjensen@hotmail.com <rasmusdalhoffjensen@hotmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-19 13:10:47 +0000

    Developer Name: Rasmus D-J Synopsis: Changes

--- PARROT/src/dk/aau/cs/giraf/parrot/BoxDragListener.java
+++ PARROT/src/dk/aau/cs/giraf/parrot/BoxDragListener.java
@@ -9,25 +9,38 @@ import android.widget.LinearLayout;
 public class BoxDragListener implements OnDragListener
 {
 	boolean insideOfMe = false;
+	boolean previous = true;
 	public boolean onDrag(View self, DragEvent event) {
 		if (event.getAction() == DragEvent.ACTION_DRAG_STARTED){
 			//Dummy
 		} else if (event.getAction() == DragEvent.ACTION_DRAG_ENTERED){ 
 			insideOfMe = true;
+			previous = false;
 		} else if (event.getAction() == DragEvent.ACTION_DRAG_EXITED){
 			insideOfMe = false;
+			previous = false;
 		} else if (event.getAction() == DragEvent.ACTION_DROP){
 			if (insideOfMe){
-				View view = (View) event.getLocalState();
-				ViewGroup owner = (ViewGroup) view.getParent();
-				owner.removeView(view);
-				LinearLayout container = (LinearLayout) self;
-				if (container.getChildCount() > 0){
-					container.addView(view, container.getChildCount());
-				} else {
-					container.addView(view);
+				if(previous == false)
+				{
+					View view = (View) event.getLocalState();
+					LinearLayout container = (LinearLayout) self;
+					if (container.getChildCount() > 0){
+						container.addView(view, container.getChildCount());
+					} else {
+						container.addView(view);
+					}
 				}
-				
+//				View view = (View) event.getLocalState();
+//				ViewGroup owner = (ViewGroup) view.getParent();
+//				owner.removeView(view);
+//				LinearLayout container = (LinearLayout) self;
+//				if (container.getChildCount() > 0){
+//					container.addView(view, container.getChildCount());
+//				} else {
+//					container.addView(view);
+//				}
+//				
 				
 				
 			}
