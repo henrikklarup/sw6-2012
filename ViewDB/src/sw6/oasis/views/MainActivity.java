7ca01e0420c3e6130004ebf3c80afb96dce50976
jensmohr90@gmail.com <jensmohr90@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-19 06:41:19 +0000

    ændringer til viewDB

--- ViewDB/src/sw6/oasis/views/MainActivity.java
+++ ViewDB/src/sw6/oasis/views/MainActivity.java
@@ -33,10 +33,23 @@ public class MainActivity extends FragmentActivity {
 	 */
 	private void initialisePaging() {
 		List<Fragment> fragments = new Vector<Fragment>();
-		fragments.add(Fragment.instantiate(this, ViewApps.class.getName()));
-		fragments.add(Fragment.instantiate(this, ViewDepartments.class.getName()));
-		fragments.add(Fragment.instantiate(this, ViewMedia.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewAuthUsers.class.getName()));
 		fragments.add(Fragment.instantiate(this, ViewProfiles.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewHasGuardian.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewDepartments.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewHasSubDepartment.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewHasDepartment.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewApps.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewListOfApps.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewMedia.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewHasLink.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewMediaProfileAccess.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewMediaDepartmentAccess.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewTags.class.getName()));
+		fragments.add(Fragment.instantiate(this, ViewHasTag.class.getName()));
+		
+		fragments.add(Fragment.instantiate(this, ViewMedia.class.getName()));
+		
 		this.mPagerAdapter = new MyPagerAdapter(super.getSupportFragmentManager(), fragments);
 
 		ViewPager pager = (ViewPager) super.findViewById(R.id.viewpager);
