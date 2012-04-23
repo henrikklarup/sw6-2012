385377f01b73a1be0a39b3ae546a95d571b2e9b4
kas.m.andersen@gmail.com <kas.m.andersen@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-23 09:55:26 +0000

    Tools-class created, applied to AuthenticationActivity

--- branches/launcher/launcher/src/dk/aau/cs/giraf/launcher/AuthenticationActivity.java
+++ branches/launcher/launcher/src/dk/aau/cs/giraf/launcher/AuthenticationActivity.java
@@ -1,17 +1,13 @@
 package dk.aau.cs.giraf.launcher;
 
-import java.util.Date;
-
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.RectF;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -31,10 +27,7 @@ public class AuthenticationActivity extends CaptureActivity {
 	private GButton gLoginButton;
 	private TextView loginNameView;
 	private TextView infoView;
-	private Context authActivity;
-	
-	private final String TIMERKEY = "TIMING";
-	private final String DATEKEY = "DATE";
+	private Context mContext;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -51,12 +44,10 @@ public class AuthenticationActivity extends CaptureActivity {
 			}
 		});
 		
-		ResetAuthData();
-		
 		gLoginButton = (GButton)this.findViewById(R.id.loginGButton);
 		loginNameView = (TextView)this.findViewById(R.id.loginname);
 		infoView = (TextView)this.findViewById(R.id.authentication_step1);
-		authActivity = this;
+		mContext = this;
 
 		final GButton button = (GButton) findViewById(R.id.loginGButton);
 		button.setOnClickListener(new View.OnClickListener() {
@@ -64,7 +55,7 @@ public class AuthenticationActivity extends CaptureActivity {
 				gLoginButton.setVisibility(View.INVISIBLE);
 				loginNameView.setVisibility(View.INVISIBLE);
 				infoView.setText(R.string.authentication_step1);
-				((AuthenticationActivity) authActivity).changeCamerafeedBorderColor(0xFFDD9639);
+				((AuthenticationActivity) mContext).changeCamerafeedBorderColor(0xFFDD9639);
 				
 				if (!getIntent().hasCategory("dk.aau.cs.giraf.launcher.GIRAF")) {
 					startActivity(mHomeIntent);
@@ -101,7 +92,7 @@ public class AuthenticationActivity extends CaptureActivity {
 			mHomeIntent = new Intent(AuthenticationActivity.this, HomeActivity.class);
 			mHomeIntent.putExtra("currentGuardianID", profile.getId());
 			
-			SaveDate(profile.getId());
+			Tools.saveLogInData(mContext, profile.getId());
 		} else {
 			this.changeCamerafeedBorderColor(0xFFFF0000);
 			gLoginButton.setVisibility(View.INVISIBLE);
@@ -112,34 +103,5 @@ public class AuthenticationActivity extends CaptureActivity {
 		}
 
 		this.getHandler().sendEmptyMessageDelayed(R.id.restart_preview, 500);
-	}
-	
-	/**
-	 * Saves the date and time of an authentication, and the user that was authenticated.
-	 * @param ID Profile ID of the authenticated user.
-	 */
-	private void SaveDate(Long ID) {
-		SharedPreferences sp = getSharedPreferences(TIMERKEY, 0);
-		SharedPreferences.Editor editor = sp.edit();
-		
-		Date d = new Date();
-		editor.putLong(DATEKEY, d.getTime());
-		
-		editor.putLong("currentGuardianID", ID);
-		
-		editor.commit();
-	}
-	
-	/**
-	 * Logs the current user out of the system.
-	 */
-	private void ResetAuthData() {
-		SharedPreferences sp = getSharedPreferences(TIMERKEY, 0);
-		SharedPreferences.Editor editor = sp.edit();
-		
-		editor.putLong(DATEKEY, 1);
-		editor.putLong("currentGuardianID", -1);
-		
-		editor.commit();
 	}
 }
---
+++ branches/launcher/launcher/src/dk/aau/cs/giraf/launcher/Tools.java
@@ -0,0 +1,60 @@
+package dk.aau.cs.giraf.launcher;
+
+import java.util.Date;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+/**
+ * Class for holding static methods and field, to minimize code duplication.
+ */
+public class Tools {
+	
+	public static final String TIMERKEY = "TIMING";
+	public static final String DATEKEY = "DATE";
+	
+	public static final String GUARDIANID = "currentGuardianID";
+	
+	/**
+	 * Saves data for the currently authorized log in.
+	 * @param context Context of the current activity.
+	 * @param id ID of the guardian logging in.
+	 */
+	public static void saveLogInData(Context context, Long id) {
+		SharedPreferences sp = context.getSharedPreferences(TIMERKEY, 0);
+		SharedPreferences.Editor editor = sp.edit();
+		
+		Date d = new Date();
+		editor.putLong(DATEKEY, d.getTime());
+		
+		editor.putLong(GUARDIANID, id);
+		
+		editor.commit();
+	}
+	
+	/**
+	 * Logs the current guardian out and launches the authentication activity.
+	 * @param context Context of the current activity.
+	 */
+	public static void logOut(Context context) {
+		clearLogOutData(context);
+		
+		Intent i = new Intent(context, AuthenticationActivity.class);
+		context.startActivity(i);
+	}
+	
+	/**
+	 * Clears the current data on who is logged in and when they logged in.
+	 * @param context Context of the current activity.
+	 */
+	public static void clearLogOutData(Context context) {
+		SharedPreferences sp = context.getSharedPreferences(TIMERKEY, 0);
+		SharedPreferences.Editor editor = sp.edit();
+		
+		editor.putLong(DATEKEY, 1);
+		editor.putLong(GUARDIANID, -1);
+		
+		editor.commit();
+	}
+}
