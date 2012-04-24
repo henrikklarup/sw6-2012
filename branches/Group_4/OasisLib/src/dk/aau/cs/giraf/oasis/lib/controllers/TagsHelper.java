17e4eb0014e87fbc4fcf71cb70e98035eee285f6
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-24 10:57:59 +0000

    Developer Name: Dan Møller Synopsis: TagsHelper.java has been updated

--- branches/Group_4/OasisLib/src/dk/aau/cs/giraf/oasis/lib/controllers/TagsHelper.java
+++ branches/Group_4/OasisLib/src/dk/aau/cs/giraf/oasis/lib/controllers/TagsHelper.java
@@ -36,7 +36,9 @@ public class TagsHelper {
 		Cursor c = _context.getContentResolver().query(TagsMetaData.CONTENT_URI, columns, TagsMetaData.Table.COLUMN_CAPTION + " = '" + tag.getCaption() + "'", null, null);
 		
 		if (c != null) {
-			id = c.getLong(c.getColumnIndex(TagsMetaData.Table.COLUMN_ID));
+			if (c.moveToFirst()) {
+				id = c.getLong(c.getColumnIndex(TagsMetaData.Table.COLUMN_ID));
+			}
 			c.close();
 		} else {
 			Uri uri;
