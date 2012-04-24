05d2ba95385261c021d69ee7af6892583ce9d649
sbf.aau@gmail.com <sbf.aau@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-24 12:54:38 +0000

    Developer Name: Simon BF Synopsis: DoneScreen v.2

--- branches/Timer/DoneScreenProject/src/dk/aau/cs/giraf/wombat/DoneScreenActivity.java
+++ branches/Timer/DoneScreenProject/src/dk/aau/cs/giraf/wombat/DoneScreenActivity.java
@@ -1,21 +1,25 @@
 package dk.aau.cs.giraf.wombat;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 public class DoneScreenActivity extends Activity {
     /** Called when the activity is first created. */
+	
+	/* path to the picture on the sdcard on the tablet*/
+	private final String imageInSD = "/sdcard/Pictures/donescreen.png";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        
+        /* decodes the .png and sets the picture in the imageview*/
+        Bitmap bm = BitmapFactory.decodeFile(imageInSD);
+        ImageView im = (ImageView)findViewById(R.id.done_pict);
+        im.setImageBitmap(bm);
+    }    
 }
