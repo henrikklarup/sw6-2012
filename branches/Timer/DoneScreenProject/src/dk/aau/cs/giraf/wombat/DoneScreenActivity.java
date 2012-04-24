c5a9ceae8001105828da25a3817ae84bd19d49d5
sbf.aau@gmail.com <sbf.aau@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-24 11:35:38 +0000

    Developer Name: Simon Synopsis: Arbejder videre på DoneScreen

--- branches/Timer/DoneScreenProject/src/dk/aau/cs/giraf/wombat/DoneScreenActivity.java
+++ branches/Timer/DoneScreenProject/src/dk/aau/cs/giraf/wombat/DoneScreenActivity.java
@@ -1,8 +1,15 @@
 package dk.aau.cs.giraf.wombat;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DoneScreenActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,6 +17,5 @@ public class DoneScreenActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
     }
 }
