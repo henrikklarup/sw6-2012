b08269a9180de739785c2b8001018b888d511d35
sbf.aau@gmail.com <sbf.aau@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-24 08:42:27 +0000

    Developer Name: Simon Synopsis: Start på doneScreen

---
+++ branches/Timer/DoneScreenProject/src/dk/aau/cs/giraf/wombat/DoneScreenActivity.java
@@ -0,0 +1,15 @@
+package dk.aau.cs.giraf.wombat;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+public class DoneScreenActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }
+}
