6512cf23bbf2df87f6bc58afbe92d289246bb9a3
thorkvist@gmail.com <thorkvist@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-04-26 11:27:23 +0000

    Developer Name: Thorbjørn Kvist Nielsen Synopsis: Clean-up and fix

--- savannah/src/savannah/io/CommunicationThread.java
+++ savannah/src/savannah/io/CommunicationThread.java
@@ -52,6 +52,7 @@ public class CommunicationThread extends Thread {
 				IOHandler.getInstance().displayMessage("RequestEvent created by: " + this.socket);
 				RequestEvent reqEvt = new RequestEvent(handle.XML(), this.socket);
 				EventQueue.getInstance().add(reqEvt);
+				IOHandler.getInstance().respond(this.socket, CRUD.REQUEST, "I so hope that this is going to work !!!");
 				IOHandler.getInstance().logIt(true);
 			}
 			//Ping
