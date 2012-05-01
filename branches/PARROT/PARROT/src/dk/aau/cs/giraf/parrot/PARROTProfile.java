016afc07e9d5048c54c6c2819b2981b76dbeb85c
GunmadMadman@gmail.com <GunmadMadman@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-01 12:08:50 +0000

    Developer Name: Jakob Jørgensen Synopsis: Committing my work in User Specific Tabs.

--- branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/PARROTProfile.java
+++ branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/PARROTProfile.java
@@ -14,6 +14,7 @@ public class PARROTProfile {
 	private ArrayList<Category> categories = new ArrayList<Category>();
 	private long profileID;
 	private int NumberOfSentencePictograms = 4;
+	private boolean[] rights = new boolean[3];
 	
 	//TODO add settings such as colour specification and others...
 	
@@ -74,4 +75,28 @@ public class PARROTProfile {
 	public void setNumberOfSentencePictograms(int numberOfSentencePictograms) {
 		NumberOfSentencePictograms = numberOfSentencePictograms;
 	}
+	
+	/*
+	 * setRights is save what functionalities a person is allowed to access.
+	 * Expected to be called as an users profile is being established or edited.
+	 */
+	public void setRights(int i, boolean yesOrNo)
+	{
+		this.rights[i] = yesOrNo;
+	}
+	
+	/* 
+	 * The getRights getMethod is used in Parrot Activity to determine if a given user is allowed 
+	 * to use certain functionalities in the App.
+	 * i is to determine which functionality is in question.
+	 * Remember: Make sure the order of the Taps is consistent with the order of their rights in the
+	 * 			 Rights array.
+	 * Read: Consider reading comment on tab initialization in PARROTActivity.Java
+	 */
+	public boolean getRights(int i)
+	{
+		boolean isAllowed = this.rights[i];
+		return isAllowed;
+	}
+	
 }
