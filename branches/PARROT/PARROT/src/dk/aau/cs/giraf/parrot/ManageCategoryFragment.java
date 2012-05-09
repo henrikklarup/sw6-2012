df4a9fda75dbda80714385a13b9f72a1a0d9f9cc
dracoblackie@hotmail.com <dracoblackie@hotmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-09 12:43:04 +0000

    Developer Name: kim Synopsis: updating code

--- branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/ManageCategoryFragment.java
+++ branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/ManageCategoryFragment.java
@@ -1,9 +1,16 @@
 package dk.aau.cs.giraf.parrot;
 
+import java.util.ArrayList;
+
 import parrot.Package.R;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -17,13 +24,14 @@ public class ManageCategoryFragment extends Fragment {
 	//Remembers the index of the item that is currently being dragged.
 	public static int draggedItemIndex = -1;
 	public static int catDragOwnerID =-1;
-	public static int currentCategoryId = 0;
+	public static int currentCategoryId = 0; //This is the currrent category that is chosen
 	public static PARROTProfile profileBeingModified = PARROTActivity.getUser();
+	public static ArrayList<Pictogram> categories =  new ArrayList<Pictogram>();
 	
 	public void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
-	    setContentView(R.layout.);//FIXME Create xml, and reffer to it here.
+	    parrent.setContentView(R.layout.managecategory_layout);//FIXME Create xml, and reffer to it here.
 		
 		Spinner profiles = (Spinner) parrent.findViewById(R.id.profiles); //FIXME Make xml for this to work.
 		ListView categories = (ListView) parrent.findViewById(R.id.categories); //FIXME Make xml for this to work.
@@ -35,7 +43,43 @@ public class ManageCategoryFragment extends Fragment {
 		
 		setListAdapter();
 				
-		categoriesArray.setAdapter(/*adapter*/);
+		
+		
+		categories.setAdapter(new ListViewAdapter(this, R.id.categoriesitem, profileBeingModified.getCategoryAt(currentCategoryId))); //FIXME Create xml for this
+		
+		pictograms.setAdapter(new PictogramAdapter(profileBeingModified.getCategoryAt(currentCategoryId), parrent));
+		
+		
+		categories.setOnItemLongClickListener(new OnItemLongClickListener()
+		{
+
+			public boolean onItemLongClick(AdapterView<?> arg0, View view, int position, long id)
+			{
+				draggedItemIndex = position;
+				catDragOwnerID = R.id.pictogramgrid;
+				ClipData data = ClipData.newPlainText("label", "text"); //TODO Dummy. Pictogram information can be placed here instead.
+				DragShadowBuilder shadowBuilder = new DragShadowBuilder(view);
+				view.startDrag(data, shadowBuilder, view, 0);
+				return true;
+			}
+
+		});
+		
+		pictograms.setOnItemLongClickListener(new OnItemLongClickListener()
+		{
+
+			public boolean onItemLongClick(AdapterView<?> arg0, View view, int position, long id)
+			{
+				draggedItemIndex = position;
+				catDragOwnerID = R.id.pictogramgrid;
+				ClipData data = ClipData.newPlainText("label", "text"); //TODO Dummy. Pictogram information can be placed here instead.
+				DragShadowBuilder shadowBuilder = new DragShadowBuilder(view);
+				view.startDrag(data, shadowBuilder, view, 0);
+				return true;
+			}
+
+		});
+		
 		
 		
 		
