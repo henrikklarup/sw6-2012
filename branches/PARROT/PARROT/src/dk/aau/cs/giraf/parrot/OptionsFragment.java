a90784857c6b091fcedeb7294de51c2379d89f17
civinther@gmail.com <civinther@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-09 12:34:23 +0000

    Developer Name: Synopsis:

--- branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/OptionsFragment.java
+++ branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/OptionsFragment.java
@@ -2,21 +2,33 @@ package dk.aau.cs.giraf.parrot;
 
 
 
+
+import yuku.ambilwarna.AmbilWarnaDialog;
+import yuku.ambilwarna.AmbilWarnaDialog.OnAmbilWarnaListener;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+
 
 public class OptionsFragment extends Fragment
 {
+	private CheckBox checkboxGradient;
 	
-	
+	private Activity parrent;
 	@Override
 	public void onAttach(Activity activity) 
 	{
 		super.onAttach(activity);
+		this.parrent = activity;
 		
 	}
 	
@@ -27,4 +39,25 @@ public class OptionsFragment extends Fragment
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.options_layout, container, false);
 	}
+	private void initColorButtons() {
+		
+		Button ccc = (Button) parrent.findViewById(R.id);
+	
+		
+		colorGradientButton1.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				AmbilWarnaDialog dialog = new AmbilWarnaDialog(getActivity(),
+						currSubP.timeLeftColor, new OnAmbilWarnaListener() {
+							public void onCancel(AmbilWarnaDialog dialog) {
+							}
+
+							public void onOk(AmbilWarnaDialog dialog, int color) {
+								currSubP.timeLeftColor = color;
+								setColor(colorGradientButton1.getBackground(),
+										currSubP.timeLeftColor);
+							}
+						});
+				dialog.show();
+			}
+		});
 }
