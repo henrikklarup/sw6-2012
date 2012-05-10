ba32b6ca3ddc4875223e352c3903a4a6e51cd1b4
Dan.stenholt@gmail.com <Dan.stenholt@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-10 11:37:12 +0000

    Developer Name: Dan Møller Synopsis: Added new views for the OasisApp

---
+++ branches/Group_4/OasisApp/src/dk/aau/cs/giraf/oasis/app/TabManagerAllDepartments.java
@@ -0,0 +1,96 @@
+package dk.aau.cs.giraf.oasis.app;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
+
+public class TabManagerAllDepartments extends Fragment implements OnTabChangeListener {
+
+	public static final String TAB_DEPARTMENTS = "Departments";
+
+	private View mRoot;
+	private TabHost mTabHost;
+	private int mCurrentTab;
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		mRoot = inflater.inflate(R.layout.taballdepartmentsview, null);
+		mTabHost = (TabHost) mRoot.findViewById(android.R.id.tabhost);
+		setupTabs();
+		return mRoot;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		setRetainInstance(true);
+
+		mTabHost.setOnTabChangedListener(this);
+		mTabHost.setCurrentTab(mCurrentTab);
+
+		updateTab(TAB_DEPARTMENTS, R.id.tabAD_1);
+
+		Button b = (Button) getView().findViewById(R.id.bTabApp);
+		b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				getActivity().finish();
+			}
+		});
+	}
+
+	private void setupTabs() {
+		mTabHost.setup();
+		mTabHost.addTab(newTab(TAB_DEPARTMENTS, R.string.nameLabel, R.id.tabAD_1));
+	}
+
+	private TabSpec newTab(String tag, int labelId, int tabContentId) {
+//		Log.d(TAG, "buildTab(): tag=" + tag);
+
+		View indicator = LayoutInflater.from(getActivity()).inflate(
+				R.layout.tab, (ViewGroup) mRoot.findViewById(android.R.id.tabs), false);
+		((TextView) indicator.findViewById(R.id.tvtab)).setText(labelId);
+
+		TabSpec tabSpec = mTabHost.newTabSpec(tag);
+		tabSpec.setIndicator(indicator);
+		tabSpec.setContent(tabContentId);
+		return tabSpec;
+	}
+
+	@Override
+	public void onTabChanged(String tabId) {
+//		Log.d(TAG, "onTabChanged(): tabId=" + tabId);
+		if (TAB_DEPARTMENTS.equals(tabId)) {
+			updateTab(tabId, R.id.tabAD_1);
+			mCurrentTab = 0;
+			return;
+		}
+	}
+
+	private void updateTab(String tabId, int placeholder) {
+		FragmentManager fm = getFragmentManager();
+
+		if (TAB_DEPARTMENTS.equals(tabId)) {
+			fm.beginTransaction()
+			.replace(placeholder, new MyProfileFrag(), tabId)
+			.commit();
+			return;
+		}
+	}
+}
