84e1fefdd9f993c5cc0da03e0bd05df2f8e5ab62
kristiank.flf@gmail.com <kristiank.flf@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-11 11:31:13 +0000

    Developer Name: Kristian Synopsis: DoneScreen now changes according to donescreenpicture

--- branches/Timer/DrawTest/src/dk/aau/cs/giraf/wombat/drawlib/DoneScreenActivity.java
+++ branches/Timer/DrawTest/src/dk/aau/cs/giraf/wombat/drawlib/DoneScreenActivity.java
@@ -1,53 +1,83 @@
 package dk.aau.cs.giraf.wombat.drawlib;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.ViewGroup.LayoutParams;
+import android.view.Display;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import dk.aau.cs.giraf.TimerLib.Attachment;
+import dk.aau.cs.giraf.TimerLib.Guardian;
+import dk.aau.cs.giraf.TimerLib.SplitImg;
+import dk.aau.cs.giraf.TimerLib.SubProfile;
 
 public class DoneScreenActivity extends Activity {
-    /** Called when the activity is first created. */
+	/** Called when the activity is first created. */
 	private final String imageInSD = "/sdcard/Pictures/faerdig.png";
 	private final String text = "Færdig";
-	
+	Guardian guard = Guardian.getInstance();
 	/* path to the picture on the sdcard on the tablet*/
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        //TODO: get extra data
-        
-        LinearLayout frame = new LinearLayout(this);
-        frame.setOrientation(LinearLayout.VERTICAL);
-        
-        ImageView iv = new ImageView(getApplicationContext());
-		iv.setScaleType(ScaleType.CENTER_INSIDE);
 
-		Bitmap bm = BitmapFactory.decodeFile(imageInSD);
-        iv.setImageBitmap(bm);
-        frame.addView(iv);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		  View main_layout = findViewById(android.R.id.content).getRootView();
+		  main_layout.setSystemUiVisibility(View.STATUS_BAR_HIDDEN);
+		WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+		Display disp = wm.getDefaultDisplay();
+		int frameHeight = disp.getHeight();
+		int frameWidth = disp.getWidth();
+		//TODO: get extra data
+		LinearLayout frame = new LinearLayout(this);
+		frame.setOrientation(LinearLayout.HORIZONTAL);
+		ImageView i = null;
+		ImageView i2 = null;
+		SubProfile sub = guard.getSubProfile();
 		
-		TextView tv = new TextView(getApplicationContext());
-		LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-		tv.setLayoutParams(lp);
-		tv.setGravity(Gravity.CENTER);
-		tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 80);
-		tv.setTextColor(0xFFFFFFFF);
-		tv.setText(text);
-		frame.addView(tv);
-        setContentView(frame);
-		
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
-    }    
+		if(sub.getDoneArt() != null){
+			switch(sub.getDoneArt().getForm()){
+			case SingleImg:
+				i = new ImageView(this);
+				i.setImageResource(sub.getDoneArt().getImg().getPath());
+				i.setBackgroundColor(000);
+				frame.addView(i, frameWidth, frameHeight);
+				break;
+			case SplitImg:
+				frameWidth = frameWidth/2;
+				i = new ImageView(this);
+				i.setImageResource(sub.getDoneArt().getLeftImg().getPath());
+				i.setBackgroundColor(000);
+				frame.addView(i, frameWidth, frameHeight);
+
+				i2 = new ImageView(this);
+				i2.setImageResource(sub.getDoneArt().getRightImg().getPath());
+				i2.setBackgroundColor(000);
+				frame.addView(i2, frameWidth, frameHeight);
+				break;
+			}
+		} else {
+        	i = new ImageView(this);
+			i.setImageResource(guard.ArtList.get(0).getPath());
+			i.setBackgroundColor(000);
+			frame.addView(i, frameWidth, frameHeight);
+		}
+
+
+		//		TextView tv = new TextView(getApplicationContext());
+		//		LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+		//		tv.setLayoutParams(lp);
+		//		tv.setGravity(Gravity.CENTER);
+		//		tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 80);
+		//		tv.setTextColor(0xFFFFFFFF);
+		//		tv.setText(text);
+		//		frame.addView(tv);
+		setContentView(frame);
+
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+	}    
 }
