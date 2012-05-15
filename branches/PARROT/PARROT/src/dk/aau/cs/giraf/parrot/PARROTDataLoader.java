d437bcc3878574991f7d6f20bb118501699e470f
GunmadMadman@gmail.com <GunmadMadman@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-15 09:26:25 +0000

    Developer Name: Jakob Jørgensen Synopsis: Fixed to utilize certain Pictograms

--- branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/PARROTDataLoader.java
+++ branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/PARROTDataLoader.java
@@ -2,10 +2,7 @@ package dk.aau.cs.giraf.parrot;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import android.R.bool;
 import android.app.Activity;
-import android.os.Bundle;
 import dk.aau.cs.giraf.oasis.lib.Helper;
 import dk.aau.cs.giraf.oasis.lib.models.App;
 import dk.aau.cs.giraf.oasis.lib.models.Media;
@@ -289,27 +286,47 @@ public class PARROTDataLoader {
 		Pictogram børsteTænderPic = new Pictogram("Børste Tænder", "/sdcard/Pictogram/Børste_Tænder.png", null, "/sdcard/Pictogram/børste_tænder.wma");
 		børsteTænderPic.setNewPictogram(true);
 		Pictogram drikkePic = new Pictogram("Drikke", "/sdcard/Pictogram/Drikke.png", null, "/sdcard/Pictogram/drikke.wma");
+		drikkePic.setNewPictogram(true);
 		Pictogram duPic = new Pictogram("Du", "/sdcard/Pictogram/Du.png", null, "/sdcard/Pictogram/du.wma");
+		duPic.setNewPictogram(true);
 		Pictogram filmPic = new Pictogram("Film", "/sdcard/Pictogram/Film.png", null, "/sdcard/Pictogram/film.wma");
+		filmPic.setNewPictogram(true);
 		Pictogram forHøjtPic = new Pictogram("For Højt", "/sdcard/Pictogram/For_Højt.png", null, "/sdcard/Pictogram/for_højt.wma");
+		forHøjtPic.setNewPictogram(true);
 		Pictogram gåPic = new Pictogram("Gå", "/sdcard/Pictogram/Gå.png", null, "/sdcard/Pictogram/gå.wma");
+		gåPic.setNewPictogram(true);
 		Pictogram jaPic = new Pictogram("Ja", "/sdcard/Pictogram/Ja.png", null, "/sdcard/Pictogram/ja.wma");
+		jaPic.setNewPictogram(true);
 		Pictogram kørePic = new Pictogram("Køre", "/sdcard/Pictogram/Køre.png", null, "/sdcard/Pictogram/køre.wma");
+		kørePic.setNewPictogram(true);
 		Pictogram laveMadPic = new Pictogram("Lave Mad", "/sdcard/Pictogram/Lave_Mad.png", null, "/sdcard/Pictogram/lave_mad.wma");
+		laveMadPic.setNewPictogram(true);
 		Pictogram legePic = new Pictogram("Lege", "/sdcard/Pictogram/Lege.png", null, "/sdcard/Pictogram/lege.wma");
+		legePic.setNewPictogram(true);
 		Pictogram migPic = new Pictogram("Mig", "/sdcard/Pictogram/Mig.png", null, "/sdcard/Pictogram/mig.wma");
 		migPic.setNewPictogram(true);
 		Pictogram morgenRoutinePic = new Pictogram("Morgen Routine", "/sdcard/Pictogram/Morgen_Routine.png", null, "/sdcard/Pictogram/morgen_routine.wma");
+		morgenRoutinePic.setNewPictogram(true);
 		Pictogram nejPic = new Pictogram("Nej", "/sdcard/Pictogram/Nej.png", null, "/sdcard/Pictogram/nej.wma");
+		nejPic.setNewPictogram(true);
 		Pictogram sePic = new Pictogram("Se", "/sdcard/Pictogram/Se.png", null, "/sdcard/Pictogram/se.wma");
+		sePic.setNewPictogram(true);
 		Pictogram sideNedPic = new Pictogram("Side Ned", "/sdcard/Pictogram/Side_Ned.png", null, "/sdcard/Pictogram/side_ned.wma");
-		Pictogram SpilleComputerPic = new Pictogram("Spille Computer", "/sdcard/Pictogram/Spille_Computer.png", null, "/sdcard/Pictogram/spille_computer.wma");
+		sideNedPic.setNewPictogram(true);
+		Pictogram spilleComputerPic = new Pictogram("Spille Computer", "/sdcard/Pictogram/Spille_Computer.png", null, "/sdcard/Pictogram/spille_computer.wma");
+		spilleComputerPic.setNewPictogram(true);
 		Pictogram stopPic = new Pictogram("Stop", "/sdcard/Pictogram/Stop.png", null, "/sdcard/Pictogram/stop.wma");
+		stopPic.setNewPictogram(true);
 		Pictogram sultenPic = new Pictogram("Sulten", "/sdcard/Pictogram/Sulten.png", null, "/sdcard/Pictogram/sulten.wma");
+		sultenPic.setNewPictogram(true);
 		Pictogram søvnigPic = new Pictogram("Søvnig", "/sdcard/Pictogram/Søvnig.png", null, "/sdcard/Pictogram/søvnig.wma");
+		søvnigPic.setNewPictogram(true);
 		Pictogram taleSammenPic = new Pictogram("Tale Sammen", "/sdcard/Pictogram/Tale_Sammen.png", null, "/sdcard/Pictogram/tale_sammen.wma");
+		taleSammenPic.setNewPictogram(true);
 		Pictogram tørstigPic = new Pictogram("Tørstig", "/sdcard/Pictogram/Tørstig.png", null, "/sdcard/Pictogram/tørstig.wma");
+		tørstigPic.setNewPictogram(true);
 		Pictogram væreStillePic = new Pictogram("Være Stille", "/sdcard/Pictogram/Være_Stille.png", null, "/sdcard/Pictogram/være_stille.wma");
+		væreStillePic.setNewPictogram(true);
 
 		Category tempCat3 = new Category(2, migPic);
 
@@ -329,7 +346,7 @@ public class PARROTDataLoader {
 		tempCat3.addPictogram(nejPic);
 		tempCat3.addPictogram(sePic);
 		tempCat3.addPictogram(sideNedPic);
-		tempCat3.addPictogram(SpilleComputerPic);
+		tempCat3.addPictogram(spilleComputerPic);
 		tempCat3.addPictogram(stopPic);
 		tempCat3.addPictogram(sultenPic);
 		tempCat3.addPictogram(søvnigPic);
