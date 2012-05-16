c27922eaf09a1d352603072982e48029a26f6a62
dracoblackie@hotmail.com <dracoblackie@hotmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-16 12:38:13 +0000

    Developer Name: kim Synopsis: update

--- branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/ManagementBoxDragListener.java
+++ branches/PARROT/PARROT/src/dk/aau/cs/giraf/parrot/ManagementBoxDragListener.java
@@ -77,19 +77,17 @@ public class ManagementBoxDragListener implements OnDragListener
 				}
 				else if(self.getId()==R.id.pictograms && ManageCategoryFragment.catDragOwnerID == R.id.categories) //We are to copy a category into another category
 				{	
-					ListView categories = (ListView) parrent.findViewById(R.id.categories);
 					
-					Category categoryCopiedFrom = ManageCategoryFragment.profileBeingModified.getCategoryAt(ManageCategoryFragment.draggedItemIndex);
+					Category categoryCopiedFrom = ManageCategoryFragment.profileBeingModified.getCategoryAt(ManageCategoryFragment.draggedItemIndex); 
 					
-					Category temp;
+					Category temp = ManageCategoryFragment.profileBeingModified.getCategoryAt(ManageCategoryFragment.currentCategoryId);
 					
 					for(int i = 0; i < categoryCopiedFrom.getPictograms().size(); i++)
 					{
-						temp = ManageCategoryFragment.profileBeingModified.getCategoryAt(ManageCategoryFragment.currentCategoryId);
 						temp.addPictogram(categoryCopiedFrom.getPictogramAtIndex(i)); 
-						ManageCategoryFragment.profileBeingModified.setCategoryAt(ManageCategoryFragment.currentCategoryId, temp);
 					}
 					
+					ManageCategoryFragment.profileBeingModified.setCategoryAt(ManageCategoryFragment.currentCategoryId, temp);
 					pictograms.setAdapter(new PictogramAdapter(ManageCategoryFragment.profileBeingModified.getCategoryAt(ManageCategoryFragment.currentCategoryId), parrent));
 				}
 				else if(self.getId()==R.id.trash && ManageCategoryFragment.catDragOwnerID == R.id.categories) //We are to delete a category
