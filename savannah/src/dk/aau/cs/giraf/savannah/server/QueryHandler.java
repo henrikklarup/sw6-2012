c47003dca60afe1465bcc38d8c5ed452ab09e12b
mfjordvald@gmail.com <mfjordvald@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-22 23:20:22 +0000

    yarra!

--- savannah/src/dk/aau/cs/giraf/savannah/server/QueryHandler.java
+++ savannah/src/dk/aau/cs/giraf/savannah/server/QueryHandler.java
@@ -9,7 +9,10 @@ public class QueryHandler {
 	public QueryHandler() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
 	{
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		con = DriverManager.getConnection("jdbc:mysql://172.25.11.65:3306/04","eder","123456");
+		con = DriverManager.getConnection("jdbc:mysql://172.25.11.65:3306/04","eder","123456"); //This needs to be changed, to anyone who may read
+																								  //this and think, "WTF, clear text password in a public project!?!?!"
+																								  //don't worry, this is strictly a testing database
+																								  //which is blocked from external access anyway..:-)
 	}
 	
 	public int SendCommit(String s)
