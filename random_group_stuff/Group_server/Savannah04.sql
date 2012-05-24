ce4e40bf3ad682c8c0cf379207c1883e2345a70c
jesper.bromose@gmail.com <jesper.bromose@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-24 09:06:11 +0000

    Developer Name: Synopsis:

--- random_group_stuff/Group_server/Savannah04.sql
+++ random_group_stuff/Group_server/Savannah04.sql
@@ -31,7 +31,7 @@ drop table `04`.`Tags`;
 
 CREATE  TABLE `04`.`AuthUsers` (
 
-  `certificate` VARCHAR(45) NOT NULL ,
+  `certificate` VARCHAR(512) NOT NULL ,
 
   `idUser` INT NOT NULL AUTO_INCREMENT ,
   
@@ -92,7 +92,7 @@ CREATE  TABLE `04`.`Profile` (
   
   FOREIGN KEY (`idProfile` )
 
-  REFERENCES `04`.`AuthUsers` (`idUser` ),
+  REFERENCES `04`.`AuthUsers` (`idUser` ) on delete cascade,
 
   PRIMARY KEY (`idProfile`) );
 
@@ -112,7 +112,7 @@ CREATE  TABLE `04`.`ListOfApps` (
   
   FOREIGN KEY (`idProfile` )
 
-  REFERENCES `04`.`Profile` (`idProfile` ),
+  REFERENCES `04`.`Profile` (`idProfile` ) ON DELETE CASCADE,
 
   PRIMARY KEY (`idApp`,`idProfile`) );
 
@@ -142,7 +142,7 @@ CREATE  TABLE `04`.`HasDepartment` (
   
   FOREIGN KEY (`idProfile` )
 
-  REFERENCES `04`.`Profile` (`idProfile` ),
+  REFERENCES `04`.`Profile` (`idProfile` ) ON DELETE CASCADE,
 
   FOREIGN KEY (`idDepartment` )
 
@@ -159,11 +159,11 @@ CREATE  TABLE `04`.`HasGuardian` (
   
   FOREIGN KEY (`idGuardian` )
 
-  REFERENCES `04`.`Profile` (`idProfile` ),
+  REFERENCES `04`.`Profile` (`idProfile` ) on delete cascade,
   
   FOREIGN KEY (`idChild` )
 
-  REFERENCES `04`.`Profile` (`idProfile` ),
+  REFERENCES `04`.`Profile` (`idProfile` ) on delete cascade,
 
   PRIMARY KEY (`idGuardian`,`idChild`) );
 
@@ -199,7 +199,7 @@ CREATE  TABLE `04`.`Media` (
   
   FOREIGN KEY (`OwnerID` )
 
-  REFERENCES `04`.`Profile` (`idProfile` ),
+  REFERENCES `04`.`AuthUsers` (`idUser` )  ON DELETE CASCADE,
 
   PRIMARY KEY (`idMedia`) );
 
@@ -211,7 +211,7 @@ CREATE  TABLE `04`.`HasTag` (
   
   FOREIGN KEY (`idMedia` )
 
-  REFERENCES `04`.`Media` (`idMedia` ),
+  REFERENCES `04`.`Media` (`idMedia` ) on delete cascade,
   
   FOREIGN KEY (`idTag` )
 
@@ -227,11 +227,11 @@ CREATE  TABLE `04`.`HasLink` (
   
   FOREIGN KEY (`idMedia` )
 
-  REFERENCES `04`.`Media` (`idMedia` ),
+  REFERENCES `04`.`Media` (`idMedia` ) on delete cascade,
   
   FOREIGN KEY (`idSubMedia` )
 
-  REFERENCES `04`.`Media` (`idMedia` ),
+  REFERENCES `04`.`Media` (`idMedia` ) on delete cascade,
 
   PRIMARY KEY (`idMedia`, `idSubMedia`) );
 
@@ -243,11 +243,11 @@ CREATE  TABLE `04`.`MediaProfileAccess` (
   
   FOREIGN KEY (`idProfile` )
 
-  REFERENCES `04`.`Profile` (`idProfile` ),
+  REFERENCES `04`.`Profile` (`idProfile` ) ON DELETE CASCADE,
   
   FOREIGN KEY (`idMedia` )
 
-  REFERENCES `04`.`Media` (`idMedia` ),
+  REFERENCES `04`.`Media` (`idMedia` ) ON DELETE CASCADE,
 
   PRIMARY KEY (`idProfile`, `idMedia`) );
 
@@ -267,43 +267,121 @@ CREATE  TABLE `04`.`MediaDepartmentAccess` (
 
   PRIMARY KEY (`idDepartment`, `idMedia`) );
 
-select * from Apps;
-select * from ListOfApps;
-select * from Profile;
-select * from AuthUsers;
 insert into Apps values(1,'TestAppe1','1.0.0', 'null', 'null', 'null');
 insert into Apps values(2,'TestAppe2','1.0.0', 'null', 'null', 'null');
 
-insert into AuthUsers values ('abekat',null,0,'Jesper','tester1');
-insert into AuthUsers values ('Dep:111',null,1,'randomString','randomString');
-insert into AuthUsers values ('Pæd:',null,0,'pæd','123');
-insert into AuthUsers values ('Gaurd',null,0,'G','1');
-insert into AuthUsers values ('Gaurd2',null,0,'Toke','Loke');
-insert into AuthUsers values ('Dep2',null,0,'Dep2','Loke');
-insert into AuthUsers values ('Subdep1',null,0,'SD1','SD1');
-insert into AuthUsers values ('SubSubdep1',null,0,'SSD1','SSD1');
-insert into Tags values(1,'Bil');
-insert into Profile values(1,'Jesper','Bromose',null,1,12345678,'dr.dk','<XML>STEERINGS</XML>');
-insert into Profile values(3,'Foo','Bar',null,0,12212121,'dr.dk','<XML>STEERINGS</XML>');
-insert into Profile values(4,'Guardian1','Guardian2',null,1,12212121,'dr.dk','<XML>STEERINGS</XML>');
-insert into Profile values(5,'TOKE','TOKEN',null,1,12212121,'dr.dk','<XML>STEERINGS</XML>');
+/* Users id=1 - 10 */
+insert into AuthUsers values ('jkkxlagqyrztlrexhzofekyzrnppajeobqxcmunkqhsbrgpxdtqgygnmbhrgnpphaxsjshlpupgakmirhpyfaivvtpynqarxsghhilhkqvpelpreevykxurtppcggkzfaepihlodgznrmbrzgqucstflhmndibuymmvwauvdlyqnnlxkurinuypmqypspmkqavuhfwsh',null,0,'User01','User01');
+insert into AuthUsers values ('ldsgjrtvuiwclpjtuxysmyjgpzsqnrtbwbdmgpalnlwtxzdubhbbtkioukaiwgbebhwovfuevykgbbnktnbzhxwugnkkllgjyovisfzzghyuqvxaoscblwqtvujqzgctslihoqetymxfupblcegpfjrfzyrfnjwevgeimxkrdixocyqmaxmyelptofyrsrtrggffmgak',null,0,'User02','User02');
+insert into AuthUsers values ('atntrqkxlnftvxquwrwcdloaumdhzkgyglezzsebpvnethrlstvmlorrolymdynjcyonkrtvcuagwigdqqkftsxxhklcnbhznthcqjxnjzzdoqvmfdlxrudcyakvrnfcbohdumawlwmfndjascmvrsoxfjgwzhdvcvqcroxoyjeazmxtrjtlkldoevgdrqvgfbklhtgm',null,0,'User03','User03');
+insert into AuthUsers values ('juuaygujymvacvldvhgirtvtumbdtfhmthtumpgqjvlhzvpzmwezifupvfhpjermlckxdvmjfpmqfadepwdvgdtwvoqkruyeuklsrurgirioqiqdzdxnbuemdmezycyncjqkvcjhgfusfggckxaispgbrzcxmtrgztnbshucxpaoodjvqujhwyeccnsxfjgkrjfoszvu',null,0,'User04','User04');
+insert into AuthUsers values ('qyrmohbmcnsljhknvggvcdifarowqvpckzxfvlkwnglztjormumiroifttxbqzmybyvbulrvnoxrdidieoxeeayxkohqrwapdnszdnnegsgdnwdoenjlwcgurjtvmufwhjfnkcpyalzkrvzmspdliaodnlkookaszjyurwjclxufomktgucbsaknxztrpkhxutbelrrc',null,0,'User05','User05');
+insert into AuthUsers values ('bphiomxvbsricewxcpuzpdtqjdcywlaplsmzjqzayhdyxeawyaeeofkpvfhwaudzwaafihtfuddsbrjhuztepopztbdgcokafnrgqrbaydsryfianltscyitukssklazgubhkdvvjqolmwiyyhuidhyqxoxwabmvdnnxatvzhvkawyiktbswjdcqlustzermuytgqvae',null,0,'User06','User06');
+insert into AuthUsers values ('qldstjxxvbdacxfqjfwbjysjzmuobkajrdnofbtewuwpfkrobhqeblvpolnwtrhxiovuepqgssemakkjvpqoworokauseymbhafvmyhcnpdfxvpevsnjvbcwzlbordoaifgjixztsadmhldzbnvbaaxvmhssijnhvrqfretxqxhxvxsjuwcknxbktfigctbwppndwxpj',null,0,'User07','User07');
+insert into AuthUsers values ('duzogdegzhtazsqmjwmxfktmnqcbpuxuvgxmbhpkzcnomoxrtrqlfisqdvfmnhmrmssocxifquqtfnzczzznunywesepobaiikgzlaecairmrlcqzdtfrxmispgamrwwcgzvlfnaysrexwdtmhytgpnncelikvrfozixdtsixwipnfactxywyeqvjhosqwekdnbcbcac',null,0,'User08','User08');
+insert into AuthUsers values ('wzkbaaogonrkgckgfrjrwdvklpcwpmloamhlfqmytotpqkrixkwqnqamazcbybhjfdalsvqpdpiwlyctcuvtyclgreonxkqqevokjdbjwdcrgkhozleidpnoiwkdmcaylmosmwbfsrcnmlwlstgfljfwdgodinjrjrygeurrxyjpudsqukqkdgwwerlotgafhqhlxszv',null,0,'User09','User09');
+insert into AuthUsers values ('osrvixzwyklicmkcwymiccawlbgctvigycafvftciuznhqrrztnyoafqrfuskqdbddrrppnadthngsfsdvooybjfwdfcdzxfdpzyvaxibxcbqnebgifdusgldvdkeonkvdmcmwffghreolhfxhrwgcogpsfayzxsoeyqwddposjdqwwiovnwabefudybzapihunhluaj',null,0,'User10','User10');
+/* Dept id=11 - 14 */
+insert into AuthUsers values ('cbtjtjbzzfqtsdjcosdudmssfwgnfyfrcpkffxzqxkgvypdkgreaaluctwpjpfyiteismfqtinhcdfxtpmggieevbnawmlfvkykfcmfwblxwfzozdxcdqifuahshoyhsuigdnhzexuaaoqwsfyalxgsvwktdqqjfuaxfgebgkwhghtkkjmdtaxquxmoqfjaibehimitsk',null,0,'Dep01','Dep01');
+insert into AuthUsers values ('uawkenuccikpredvjqzvstrxhrajeygolljxoylbryhzytglxgldfnzeoybifokrdbdvkuscqfoutwemarihovwzibzrfmiihmevjfblrclcnrqmerghcddmdvaulyosrjyyebtrlgiqjquwghzsobflpmeerxpllzhknmcxgmxiaxhemhzkxleeipiecwtqlopvigbda',null,0,'Dep02','Dep02');
+insert into AuthUsers values ('qzwogtzadtzpcundjsnanlwqffzeyrqdcucxtsnogedpmnosrfqanoyikaamsbucfhmqjefffijypdewwrnlpkbnaqlcciklgdmliqyllqcuysicmeqxvcvhlnvfyntrmeuaumxmdldbxkrksmkitisachsrcymnaiytxgysecphnirqlzgttlvuhtkvtqnezfimjqkbl',null,0,'Dep03','Dep03');
+insert into AuthUsers values ('rljqzvcnzpiwfnvgafaybthfwcygnxrudukwxtdoezunoithgozkslqbcxyfaegjxadpvrsnsyebjixwiaxvkydumidpjoznvfeumsumizyeoywjftbymwhvazfxpkegvcqwiujwgsyfttqyiejxgxxukaqozonylkbilgurcjyjcpgxjommrsgrpftwrgpowekiepvio',null,0,'Dep04','Dep04');
+/*SubDep  15-17 */
+insert into AuthUsers values ('fslxiwoueeuuxlkjgaedgtvvqwnatnfqitdcojzgkqwpnofjnnwhtatwfnlurryxhgzzkzjpnrexhxtdrskyaevneoxzmenomrrgaglgudqwuijlrdyhlxntfegxrkjznadeqznpvqgcrigjlemkkiuxlamqjfudfwtxzdzsonqfseyoqozshkzyzxzubllqwfharcoel',null,0,'SubDep01','SubDep01');
+insert into AuthUsers values ('ztztmzmovohlcibviudflauhapraotxxypvudxowxhlcnawvestngldswvysfparhkpvxatiegqhirqtxsfexbjhpwyhkxkgfztqtjypzdalkdqwogyqrnwqxqfrhhwozxodfyzjkfsiutqwhtmfyqzaktzsrsxzdatubddecrkjcuptqesgtawztdmvvtzmthqsyqjfx',null,0,'SubDep02','SubDep02');
+insert into AuthUsers values ('dqxpsusjiricqjidmmnufgbmdkmmxxorqyucyskgdbmugixverflxfxrevtcgyvqqjfavvkskxizcsfdyfegkwbpmdabygsekpoumjptcyimujrtlczaapjdleuykfztjnqqhubxupjyjruhtbnqjyhljoipwrizvrslqdoktwoeguurnvpxpqhupnwtgjaudrrloopcr',null,0,'SubDep03','SubDep03');
 
-insert into Department values (2,'Skolebakken','Vejlevej 1',12345678,'vejle@dr.dk');
-insert into Department values (6,'Sebvej','TROLOLOL 1',19857463,'seb@seb.dk');
-insert into Department values (7,'Subdep1','SD1',12345678,'SD1');
-insert into Department values (8,'SubSubdep1','SD1',12345678,'SD1');
-insert into HasDepartment values (1,2);
-insert into HasDepartment values (3,2);
-insert into HasSubDepartment values(2,7);
-insert into HasSubDepartment values(7,8);
+insert into Profile values(1,'User01','User01',null,2,12345678,null,'<XML>Settings</XML>');
+insert into Profile values(2,'User02','User02',null,2,12345678,null,'<XML>Settings</XML>');
+insert into Profile values(3,'User03','User03',null,2,12345678,null,'<XML>Settings</XML>');
+insert into Profile values(4,'User04','User04',null,2,12345678,null,'<XML>Settings</XML>');
+insert into Profile values(5,'User05','User05',null,2,12345678,null,'<XML>Settings</XML>');
+insert into Profile values(6,'User06','User06',null,2,12345678,null,'<XML>Settings</XML>');
+insert into Profile values(7,'User07','User07',null,2,12345678,null,'<XML>Settings</XML>');
+insert into Profile values(8,'User08','User08',null,2,12345678,null,'<XML>Settings</XML>');
+insert into Profile values(9,'User09','User09',null,2,12345678,null,'<XML>Settings</XML>');
+insert into Profile values(10,'User10','User10',null,2,12345678,null,'<XML>Settings</XML>');
 
-insert into ListOfApps values(1,1,null,null);
-insert into ListOfApps values(2,1,null,null);
+insert into Department values(11,'Birken','Dept01',12345678,'Dept@dept.com');
+insert into Department values(12,'Egebakken','Dept02',12345678,'Dept@dept.com');
+insert into Department values(13,'Talepædagog','Dept03',12345678,'Dept@dept.com');
+insert into Department values(14,'Dept04','Dept04',12345678,'Dept@dept.com');
+
+insert into Department values(15,'SubDept01','SubDept01',12345678,'Dept@dept.com');
+insert into Department values(16,'SubDept01','SubDept01',12345678,'Dept@dept.com');
+insert into Department values(17,'SubDept01','SubDept01',12345678,'Dept@dept.com');
+
+
+insert into Tags values(null,'Bil');
+insert into Tags values(null,'Kat');
+insert into Tags values(null,'Abe');
+insert into Tags values(null,'Hund');
+insert into Tags values(null,'Mand');
+insert into Tags values(null,'Sko');
+insert into Tags values(null,'Sok');
+insert into Tags values(null,'Ål');
+insert into Tags values(null,'Taske');
+insert into Tags values(null,'Sang');
+insert into Tags values(null,'Mor');
+insert into Tags values(null,'Far');
+insert into Tags values(null,'Søster');
+insert into Tags values(null,'Bror');
+
+select * from AuthUsers;
+
+select * from HasLink where idMedia = 5 and idSubMedia = 4;
+
+DELETE FROM HasLink WHERE idMedia=5 and idSubMedia=4;
+select * from Media
+								where idMedia in (select idMedia from MediaProfileAccess
+									where idProfile = 1) or ownerID = 1;
+
+select * from Media
+    where name like '%d%' or idMedia in (select idMedia from HasTag
+                                            where idTag in (select idTag from Tags
+                                                            where caption like '%d%'));
+SELECT * from Media where mPublic = 1;
+select * from HasTag;
+select * from Media
+    where idMedia in (select idMedia from HasTag
+                        where idTag = 3) and (select idMedia from HasTag
+                        where idTag = 4);
+
+SELECT * from Media
+where idMedia in (select idMedia from MediaProfileAccess where idProfile =4);
+
+select * from MediaProfileAccess;
+
+select * from MediaProfileAccess;
+insert into MediaProfileAccess values(3,2);
+
+select * from MediaProfileAccess;
+select * from AuthUsers;
+select * from Tags;
+
+select * from Profile;
+select * from HasDepartment;
+select * from ListOfApps;
+select * from HasGuardian;
+delete from AuthUsers where idUser = 1;
 
 select * from Department 
 						where idDepartment not in 
 						(select idSubDepartment from HasSubDepartment);
-                        
+                      
+                      
+select idTags, caption from Tags 
+    where idTags in (select idTag from HasTag 
+                    where idMedia = 1);
+                    
+insert into MediaProfileAccess values (5,11);
+select * from Profile
+where idProfile in (select idProfile from MediaProfileAccess
+                    where idMedia = 1);
+
+select * from Tags; 
+select * from Media;
+SELECT * from Profile where idProfile = 1;                        
 Select * from Profile
 						where 
             pRole = 3 and
@@ -325,13 +403,15 @@ UPDATE Profile SET pRole = 1 WHERE idProfile = 10 ;
 
 insert into Media values(1,'c:\test','MyTest',1,'public',1);
 insert into HasTag values(1,1);
-insert into MediaProfileAccess values(1,1);
+insert into MediaProfileAccess values(5,8);
 
 insert into HasDepartment values (1,6);
 insert into HasGuardian values (1,3);
 insert into HasGuardian values (4,3);
 select * from HasGuardian;
 
+select * from Tags
+where caption like '%a';
 select * from ListOfApps
 where idProfile =1;
 
@@ -362,6 +442,8 @@ select * from Department
         (select idDepartment from HasDepartment
                  where  idProfile = '1');
 
+select * from Media where ownerID = 4;
+
 UPDATE Profile SET firstname='Tester'
     where idProfile =1;
 
@@ -369,7 +451,7 @@ select * from HasDepartment;
 
 select * from HasGuardian;
 
-select * from Department;
+select * from Tags;
 
 SELECT * from AuthUsers;
 
@@ -391,10 +473,14 @@ select * from Department
 					where idDepartment = 6);
 
 
+select * from Media 
+where idMedia in (select idMedia from MediaProfileAccess
+                    where idProfile = 4) or ownerID = 4;
  
                         
 insert into HasGuardian values(1,3);
 
+select * from MediaProfileAccess;
 select * from Profile;
 
 select password from Profile where idProfile = 7;
@@ -434,3 +520,5 @@ select * from Profile
 
 select idProfile, firstname from Profile
     where 'abekat' in (select certificate from AuthUsers);
+
+SHOW VARIABLES LIKE "%version%"
