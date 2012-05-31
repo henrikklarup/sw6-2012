81c38a35491b5c9a49033db5068057092c80e58c
henrikklarup@gmail.com <henrikklarup@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-05-31 20:56:47 +0000

    Developer Name: Henrik Synopsis: LocalDB To Trunk

---
+++ trunk/Group_admin/LocalDB/src/dk/aau/cs/giraf/oasis/localdb/AuthUsersTable.java
@@ -0,0 +1,36 @@
+package dk.aau.cs.giraf.oasis.localdb;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class AuthUsersTable {
+
+	private static final String TABLE_CREATE = "CREATE TABLE "
+			+ AuthUsersMetaData.Table.TABLE_NAME
+			+ "("
+			+ AuthUsersMetaData.Table.COLUMN_ID + " INTEGER NOT NULL, "
+			+ AuthUsersMetaData.Table.COLUMN_CERTIFICATE + " TEXT NOT NULL, "
+			+ AuthUsersMetaData.Table.COLUMN_ROLE + " INTEGER NOT NULL, "
+			+ "PRIMARY KEY (" + AuthUsersMetaData.Table.COLUMN_ID + ", " + AuthUsersMetaData.Table.COLUMN_ID + ")"
+			+ ");";
+
+	private static final String TABLE_DROP= "DROP TABLE IF EXISTS " + AuthUsersMetaData.Table.TABLE_NAME + ";";
+
+	/**
+	 * Executes sql string for creating certificate table
+	 * @param db this is an instance of a sqlite database
+	 */
+	public static void onCreate(SQLiteDatabase db) {
+		db.execSQL(TABLE_CREATE);
+	}
+
+	/**
+	 * executes a sql string which drops the old table and then the method calls oncreate, which create a new certificate table
+	 * @param db this is a instance of a sqlite database
+	 * @param oldVersion integer referring to the old version number
+	 * @param newVersion integer referring to the new version number
+	 */
+	public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL(TABLE_DROP);
+		onCreate(db);
+	}
+}
---
+++ trunk/Group_admin/LocalDB/src/dk/aau/cs/giraf/oasis/localdb/HasGuardianMetaData.java
@@ -0,0 +1,19 @@
+package dk.aau.cs.giraf.oasis.localdb;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class HasGuardianMetaData {
+
+	public static final Uri CONTENT_URI = Uri.parse("content://dk.aau.cs.giraf.oasis.localdb.AutismProvider/hasguardian");
+
+	public static final String CONTENT_TYPE_HASGUARDIANS_LIST = "vnd.android.cursor.dir/vnd.dk.hasguardian";
+	public static final String CONTENT_TYPE_HASGUARDIAN_ONE = "vnd.android.cursor.item/vnd.dk.hasguardian";
+
+	public class Table implements BaseColumns {
+		public static final String TABLE_NAME = "tbl_hasguardian";
+
+		public static final String COLUMN_IDGUARDIAN = "hasguardian_guardianid";
+		public static final String COLUMN_IDCHILD = "hasguardian_childid";
+	}
+}
