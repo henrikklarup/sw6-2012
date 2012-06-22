7ef06c65301ad18c2cbda1d5605629e71ae38e82
rasmus5034@gmail.com <rasmus5034@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-06-22 11:11:12 +0000

    Developer Name: Rasmus Aaen Synopsis: Tilføjede Kommentarer

--- wombat/src/dk/aau/cs/giraf/wombat/CustomizeFragment.java
+++ wombat/src/dk/aau/cs/giraf/wombat/CustomizeFragment.java
@@ -986,6 +986,7 @@ public class CustomizeFragment extends Fragment {
 								 if (preSubP != null) {
 									 m_savedSubprofile = currSubP.save(preSubP,
 											 true);
+									 preSubP = null;
 								 } else {
 									 m_savedSubprofile = guard.getChild().save(
 											 currSubP, false);
@@ -1022,6 +1023,7 @@ public class CustomizeFragment extends Fragment {
 						 SubProfile m_savedSubprofile;
 						 if (preSubP != null) {
 							 m_savedSubprofile = currSubP.save(preSubP, true);
+							 preSubP = null;
 						 } else {
 							 m_savedSubprofile = guard.getChild().save(currSubP,
 									 false);
@@ -1329,8 +1331,6 @@ public class CustomizeFragment extends Fragment {
 		 setDonePicture(currSubP.getDoneArt());
 	 }
 }
-/* GOING FOR LEET */
-/* GOING FOR LEET */
 /* GOING FOR LEET */
 /* GOING FOR LEET */
 /* GOING FOR LEET */
