dd4cd5d69808e35cfccd1e6581301ab4ccbd1241
rasmus5034@gmail.com <rasmus5034@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-06-22 11:04:04 +0000

    Developer Name: Synopsis:

--- branches/Timer/DrawTest/src/dk/aau/cs/giraf/wombat/drawlib/DrawLibActivity.java
+++ branches/Timer/DrawTest/src/dk/aau/cs/giraf/wombat/drawlib/DrawLibActivity.java
@@ -29,12 +29,16 @@ public class DrawLibActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);	
+		/* Hide the status bar */
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		  View main_layout = findViewById(android.R.id.content).getRootView();
 		  main_layout.setSystemUiVisibility(View.STATUS_BAR_HIDDEN);
+		  
 //		SubProfile sub = new ProgressBar("", "", 0xff3D3D3D, 0xff000066, 0xffB8B8B8, 0xff000000, 3, false);
 //		SubProfile sub2 = new ProgressBar("", "", 0xff3D3D3D, 0xff000066, 0xffB8B8B8, 0xff000000, 900, true);
 //		sub.setAttachment(sub2);
+		  
+		/* Get the guardian from timerlib */
 		Guardian guard = Guardian.getInstance();
 		SubProfile sub = guard.getSubProfile();
 		
@@ -47,30 +51,31 @@ public class DrawLibActivity extends Activity {
 				
 		
 		if (sub.getAttachment() == null) {
-			/* Set the drawing class (which extends View) as the content view */
+			/* Set the drawing class as the content view */
 			View v = genDrawView(sub,frameWidth);
 			v.setKeepScreenOn(true);
 			setContentView(v);
 		} else {
-			
+			/* Since there is an attachement do all this */
 			LinearLayout frame = new LinearLayout(this);
 			frame.setKeepScreenOn(true);
 			GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, new int[] {sub.bgcolor, 0xFF000000});
 			
+			/* Initialize views */
 			View v = null;
 			View v2 = null;
 			ImageView i = null;
 			ImageView i2 = null;
 			ImageView i3 = null;
 			switch(sub.getAttachment().getForm()){
-			case Timer:
+			case Timer: // Attach another timer
 				frameWidth = frameWidth/2;
 				 v = genDrawView(sub,frameWidth);
 				frame.addView(v, frameWidth, frameHeight);
 				v2 = genDrawView(sub.getAttachment().genSub(),frameWidth);
 				frame.addView(v2, frameWidth, frameHeight);
 				break;
-			case SingleImg:
+			case SingleImg: // Attach a single image
 				frameWidth = frameWidth/2;
 				v = genDrawView(sub,frameWidth);
 				frame.addView(v, frameWidth, frameHeight);
@@ -80,7 +85,7 @@ public class DrawLibActivity extends Activity {
 				i.setBackgroundDrawable(gd);
 				frame.addView(i, frameWidth, frameHeight);
 				break;
-			case SplitImg:
+			case SplitImg: // Attach two images
 				frameWidth = frameWidth/2;
 				v = genDrawView(sub, frameWidth);
 
@@ -104,28 +109,41 @@ public class DrawLibActivity extends Activity {
 				break;
 			}
 			
-			setContentView(frame);	
+			/* Set the content of DrawLibActivity as the above frame*/
+			setContentView(frame);
 		}
 		
+		/* Set a timer for the DoneScreenActivity*/
 		mHandler = new Handler();
 		
 		mRunnable = new Runnable() {
             public void run() {
                 final Intent mainIntent = new Intent(DrawLibActivity.this, DoneScreenActivity.class);
-                //TODO: putExtra data with picture and text information
                 DrawLibActivity.this.startActivity(mainIntent);
                 DrawLibActivity.this.finish();
             }
         };
 		
+        /* Set the delay of the intent to the time of the timer + 1 second
+         * otherwize the user will have a hard time seeing the timer reach 0*/
 		mHandler.postDelayed(mRunnable, (sub.get_totalTime()+1)*1000);
 	}
 	
+	/* When the activity is destroyed, kill the donescreen aswell*/
 	public void onDestroy(){
 		super.onDestroy();
 		mHandler.removeCallbacks(mRunnable);
 	}
 	
+	/**
+	 * Generate a view from the subprofile
+	 * @param sub
+	 * 		Subprofile which should be converted to a view
+	 * @param frameWidth
+	 * 		The width of the frame it should be withing
+	 * @return
+	 * 		A view of the subprofile
+	 */
 	private View genDrawView(SubProfile sub, int frameWidth) {
 		switch (sub.formType()) {
 		case ProgressBar:
