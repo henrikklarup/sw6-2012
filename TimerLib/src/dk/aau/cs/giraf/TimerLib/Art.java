8eabf5d21240579a81eebd07b94c5ac9ed7afd2c
kristiank.flf@gmail.com <kristiank.flf@gmail.com@c0638ea9-114b-2d6f-4d9a-f24ace1891a2>
2012-06-23 23:28:07 +0000

    Developer Name: Synopsis:

--- TimerLib/src/dk/aau/cs/giraf/TimerLib/Art.java
+++ TimerLib/src/dk/aau/cs/giraf/TimerLib/Art.java
@@ -6,25 +6,45 @@ public class Art {
 	private String _caption;
 	private int _id = -1;
 	Guardian guard = Guardian.getInstance();
-	
+	/**
+	 * Art constructor
+	 * @param path Resource path to the pictogram (R class)
+	 * @param caption Caption associated with the pictogram
+	 * @param id Id for selected a certain art object. The id need to be the same as its position in the ArtList in the Guardian class
+	 * @see Guardian#getInstance
+	 */
 	public Art(int path, String caption, int id){
 		this._path = path;
 		this._caption = caption;
 		this._id = id;
 	}
-	
+	/**
+	 * 
+	 * @return Art caption string
+	 */
 	public String getName(){
 		return this._caption;
 	}
-	
+	/**
+	 * 
+	 * @return "undefined" formFactor
+	 */
 	public formFactor getForm(){
 		return formFactor.undefined;
 	}
 	
+	/**
+	 * 
+	 * @return Resource class path to the pictogram
+	 */
 	public int getPath(){
 		return this._path;
 	}
 	
+	/**
+	 * Used to retrieve a specific art object
+	 * @return Id
+	 */
 	int getId(){
 		return this._id;
 	}
--- TimerLib/src/dk/aau/cs/giraf/TimerLib/Attachment.java
+++ TimerLib/src/dk/aau/cs/giraf/TimerLib/Attachment.java
@@ -1,53 +1,94 @@
 package dk.aau.cs.giraf.TimerLib;
 
 import java.util.HashMap;
-
+/**
+ * 
+ * Attachment is a super class, and is used for attaching an extra timer, a pictogram next to a timer, or 
+ * replacing the default pictogram on done screen
+ *
+ */
 public class Attachment {
 	
+	/**
+	 * Default constructor, only used internal in TimerLib
+	 */
 	Attachment(){
 		
 	}
 	
+	/**
+	 * 
+	 * @return this attachment
+	 */
 	public Attachment getAttachment(){
 		return this;
 	}
 	
+	/**
+	 * super method which is only implemented so inherited class can use it.
+	 * @return "undefined" formFactor
+	 */
 	public formFactor getForm(){
 		return formFactor.undefined;
 	}
 	
 	//May only be used on Timer
+	/**
+	 * Is only used on the Timer Attachment
+	 * @param t The new time
+	 */
 	void changeTime(int t){
 		
 	}
-	
+	/**
+	 * super method which is only implemented so inherited class can use it.
+	 * @return null
+	 */
 	public Art getImg(){
 		return null;
 	}
-	
+	/**
+	 * super method which is only implemented so inherited class can use it.
+	 * @return null
+	 */
 	public Art getLeftImg(){
 		return null;
 	}
-	
+	/**
+	 * super method which is only implemented so inherited class can use it.
+	 * @return null
+	 */
 	public Art getRightImg(){
 		return null;
 	}
-	
+	/**
+	 * super method which is only implemented so inherited class can use it.
+	 * @return null
+	 */
 	public SubProfile genSub(){
 		SubProfile asd = null;
 		return asd;
 	}
-	
+	/**
+	 * super method which is only implemented so inherited class can use it.
+	 * @return null
+	 */
 	public int getColor(){
 		return 0;
 	}
-	
+	/**
+	 * super method which is only implemented so inherited class can use it.
+	 * @return null
+	 */
 	public Attachment generateAttArt(HashMap map){
 		Attachment att = null;
 		
 		return att;
 	}
-	
+	/**
+	 * Generates a hashmap with default information
+	 * @return HashMap used to save in OasisLocalDatabase
+	 */
 	public HashMap getHashMap(HashMap map){
 		//Defines what kind of attachment it is
 		map.put("AttachmentForm", String.valueOf(this.getForm()));
